89e276251e576904531a02270dca3b484f9c3913
Teddi <teddikao@gmail.com>
2026-03-07 20:59:33 +0800

    Add additional state to track dialog input

--- app/src/features/variants/variantCreation/stores/createVariantDialogStore.ts
+++ app/src/features/variants/variantCreation/stores/createVariantDialogStore.ts
@@ -4,12 +4,20 @@ type CreateVariantDialogStore = {
 	isOpen: boolean;
 	openDialog: () => void;
 	closeDialog: () => void;
+
+	variantName: string;
+	updateVariantName: (name: string) => void;
+	clearVariantName: () => void;
 };
 
 const useCreateVariantDialogStore = create<CreateVariantDialogStore>((set) => ({
 	isOpen: false,
 	openDialog: () => set({ isOpen: true }),
 	closeDialog: () => set({ isOpen: false }),
+
+	variantName: "",
+	updateVariantName: (name) => set({ variantName: name }),
+	clearVariantName: () => set({ variantName: "" }),
 }));
 
 export default useCreateVariantDialogStore;
