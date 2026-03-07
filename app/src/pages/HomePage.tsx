6757da768389cf75d5f3bc7619bbedecbba8df99
Teddi <teddikao@gmail.com>
2026-03-07 12:45:14 +0800

    Add styling to root to ensure it takes up entire page

--- app/src/pages/HomePage.tsx
+++ app/src/pages/HomePage.tsx
@@ -1,8 +1,15 @@
+import {Button} from "@/components/ui/button";
+
 function HomePage() {
 	return (
 		<div className="flex flex-col items-center justify-center w-full h-full">
 			<h1 className="text-3xl">Chess++</h1>
 			<p>Create and play with your own custom chess pieces</p>
+
+			<div className="flex flex-row">
+				<Button>Create variant</Button>
+				<Button>Play variant</Button>
+			</div>
 		</div>
 	)
 }
