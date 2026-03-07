25b12f244bc985da2cefbd41664104bdeb60e942
Teddi <teddikao@gmail.com>
2026-03-07 21:03:39 +0800

    Add variant creation dialog to home page

--- app/src/pages/HomePage.tsx
+++ app/src/pages/HomePage.tsx
@@ -1,27 +1,32 @@
 import { Button } from "@/components/ui/button";
 import { IconChess, IconFolder, IconPlus } from "@tabler/icons-react";
+import CreateVariantDialog from "@/features/variants/variantCreation/components/CreateVariantDialog";
 
 function HomePage() {
 	return (
-		<div className="flex flex-col items-center justify-center w-full h-full gap-2 bg-linear-to-b from-white to-purple-400">
-			<h1 className="text-6xl font-bold">Chess++</h1>
-			<p>Create and play with your own custom chess pieces</p>
+		<>
+			<div className="flex flex-col items-center justify-center w-full h-full gap-2 bg-linear-to-b from-white to-purple-400">
+				<h1 className="text-6xl font-bold">Chess++</h1>
+				<p>Create and play with your own custom chess pieces</p>
 
-			<div className="flex flex-row gap-4">
-				<Button className="px-4">
-					<IconPlus />
-					Create variant
-				</Button>
-				<Button className="px-4">
-					<IconFolder />
-					My variants
-				</Button>
-				<Button className="px-4">
-					<IconChess />
-					Play variant
-				</Button>
+				<div className="flex flex-row gap-4">
+					<Button className="px-4">
+						<IconPlus />
+						Create variant
+					</Button>
+					<Button className="px-4">
+						<IconFolder />
+						My variants
+					</Button>
+					<Button className="px-4">
+						<IconChess />
+						Play variant
+					</Button>
+				</div>
 			</div>
-		</div>
+
+			<CreateVariantDialog />
+		</>
 	);
 }
 
