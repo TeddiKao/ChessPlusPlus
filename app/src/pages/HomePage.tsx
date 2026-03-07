26cd8559022760de8ca2f6214b991c09b5f5d984
Teddi <teddikao@gmail.com>
2026-03-07 12:51:44 +0800

    Add spacing between text on home page

--- app/src/pages/HomePage.tsx
+++ app/src/pages/HomePage.tsx
@@ -2,13 +2,13 @@ import {Button} from "@/components/ui/button";
 
 function HomePage() {
 	return (
-		<div className="flex flex-col items-center justify-center w-full h-full">
-			<h1 className="text-3xl">Chess++</h1>
+		<div className="flex flex-col items-center justify-center w-full h-full gap-2">
+			<h1 className="text-4xl font-bold">Chess++</h1>
 			<p>Create and play with your own custom chess pieces</p>
 
-			<div className="flex flex-row">
-				<Button>Create variant</Button>
-				<Button>Play variant</Button>
+			<div className="flex flex-row gap-4">
+				<Button className="px-4">Create variant</Button>
+				<Button className="px-4">Play variant</Button>
 			</div>
 		</div>
 	)
