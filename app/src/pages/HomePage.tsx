3c73978d3c8db07eda9fe006991a50e7f0a76ab3
Teddi <teddikao@gmail.com>
2026-03-07 12:58:25 +0800

    Add icons to buttons for home page

--- app/src/pages/HomePage.tsx
+++ app/src/pages/HomePage.tsx
@@ -1,14 +1,21 @@
 import {Button} from "@/components/ui/button";
+import {IconChess, IconPlus} from "@tabler/icons-react";
 
 function HomePage() {
 	return (
 		<div className="flex flex-col items-center justify-center w-full h-full gap-2">
-			<h1 className="text-4xl font-bold">Chess++</h1>
+			<h1 className="text-6xl font-bold">Chess++</h1>
 			<p>Create and play with your own custom chess pieces</p>
 
 			<div className="flex flex-row gap-4">
-				<Button className="px-4">Create variant</Button>
-				<Button className="px-4">Play variant</Button>
+				<Button className="px-4">
+					<IconPlus />
+					Create variant
+				</Button>
+				<Button className="px-4">
+					<IconChess />
+					Play variant
+				</Button>
 			</div>
 		</div>
 	)
