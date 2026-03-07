2e7dc60ae848c1079fd0d25bc0572a3284c1d190
Teddi <teddikao@gmail.com>
2026-03-07 11:00:09 +0800

    Add text to home page

--- app/src/pages/HomePage.tsx
+++ app/src/pages/HomePage.tsx
@@ -1,3 +1,10 @@
-function HomePage() {}
+function HomePage() {
+	return (
+		<div className="flex flex-col items-center justify-center w-full h-full">
+			<h1 className="text-3xl">Chess++</h1>
+			<p>Create and play with your own custom chess pieces</p>
+		</div>
+	)
+}
 
 export default HomePage;
