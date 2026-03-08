7e7cf84c920bb03d54398a2512940950171152de
Teddi <teddikao@gmail.com>
2026-03-08 11:52:04 +0800

    Add piece rules to variant info type

--- app/src/features/variants/common/types/variants.ts
+++ app/src/features/variants/common/types/variants.ts
@@ -1,24 +1,9 @@
-type PieceOwnershipRules = {
-	white: string[];
-	black: string[];
-};
-
-type SquareInfo = {
-	pieceName: string;
-	xPos: number;
-	yPos: number;
-};
-
-type SetupRules = {
-	pieceOwnership: PieceOwnershipRules;
-	boardXSize: number;
-	boardYSize: number;
-
-	startingPosition: SquareInfo[];
-};
+import type { SetupRules } from "@/features/variants/common/types/setupRules";
+import type { PiecesRules } from "@/features/variants/common/types/pieceRules";
 
 type VariantInfo = {
 	setupRules: SetupRules;
+	piecesRules: PiecesRules;
 };
 
 export type { VariantInfo };
