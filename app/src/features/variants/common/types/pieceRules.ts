39c98a99b7913c73a2c9d61969df2e1fbac49b13
Teddi <teddikao@gmail.com>
2026-03-08 11:50:59 +0800

    Create types for piece rules

---
+++ app/src/features/variants/common/types/pieceRules.ts
@@ -0,0 +1,28 @@
+type MovementConditions = "hasNotMoved";
+type MoveStopConditions = "insidePiece";
+
+type PieceMoveDefinition = {
+	moveX: number;
+	moveY: number;
+	range: number | "inf";
+	moveStopConditions: MoveStopConditions[];
+};
+
+type PieceMovementRules = {
+	forMovement: boolean;
+	forCapture: boolean;
+
+	conditions: MovementConditions[];
+	validMove: boolean;
+
+	moveDefinition: PieceMoveDefinition;
+	chainedMoves: PieceMovementRules[];
+};
+
+type PieceRules = {
+	moves: PieceMovementRules[];
+};
+
+type PiecesRules = Record<string, PieceRules>;
+
+export type { PiecesRules };
