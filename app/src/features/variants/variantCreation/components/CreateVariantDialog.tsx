868eba06fdb05d5dd073c2aebce128cf4e005d8f
Teddi <teddikao@gmail.com>
2026-03-08 10:11:43 +0800

    Add JSX markup for CreateVariantDialog

--- app/src/features/variants/variantCreation/components/CreateVariantDialog.tsx
+++ app/src/features/variants/variantCreation/components/CreateVariantDialog.tsx
@@ -1,3 +1,47 @@
-function CreateVariantDialog() {}
+import {
+	Dialog,
+	DialogContent,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import useCreateVariantDialogStore from "@/features/variants/variantCreation/stores/createVariantDialogStore";
+import type { ChangeEvent } from "react";
+import { Label } from "@/components/ui/label";
+
+function CreateVariantDialog() {
+	const { variantName, updateVariantName } = useCreateVariantDialogStore();
+
+	function handleVariantNameOnChange(e: ChangeEvent<HTMLInputElement>) {
+		updateVariantName((e.target as HTMLInputElement).value);
+	}
+
+	return (
+		<Dialog open={true}>
+			<DialogContent>
+				<DialogHeader>
+					<DialogTitle>Create variant</DialogTitle>
+				</DialogHeader>
+
+				<div className="flex flex-col gap-2">
+					<Label htmlFor="variantNameInput">Variant name</Label>
+					<Input
+						name="variantNameInput"
+						type="text"
+						placeholder="Variant name"
+						onChange={handleVariantNameOnChange}
+						value={variantName}
+					/>
+				</div>
+
+				<DialogFooter>
+					<Button className="w-full">Create variant</Button>
+				</DialogFooter>
+			</DialogContent>
+		</Dialog>
+	);
+}
 
 export default CreateVariantDialog;
