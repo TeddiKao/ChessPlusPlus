10d6d15a44b1abce3394dbafc2be2671608c9d86
Teddi <teddikao@gmail.com>
2026-03-08 21:07:54 +0800

    Create store for variant list dialog

---
+++ app/src/features/variants/variantListing/stores/variantListDialogStore.ts
@@ -0,0 +1,24 @@
+import { create } from "zustand";
+
+type VariantListDialogStore = {
+	isOpen: boolean;
+	openDialog: () => void;
+	closeDialog: () => void;
+
+	selectedVariantId: string | null;
+	updateSelectedVariantId: (variantId: string) => void;
+	clearSelectedVariantId: () => void;
+};
+
+const useVariantListDialogStore = create<VariantListDialogStore>((set) => ({
+	isOpen: false,
+	openDialog: () => set({ isOpen: true }),
+	closeDialog: () => set({ isOpen: false }),
+
+	selectedVariantId: null,
+	updateSelectedVariantId: (variantId) =>
+		set({ selectedVariantId: variantId }),
+	clearSelectedVariantId: () => set({ selectedVariantId: null }),
+}));
+
+export default useVariantListDialogStore;
