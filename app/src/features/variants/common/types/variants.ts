f75b21b2b7614f5cb536cd8c9b7e732ad9c0dd6a
Teddi <teddikao@gmail.com>
2026-03-08 11:52:36 +0800

    Rename type for clarity

--- app/src/features/variants/common/types/variants.ts
+++ app/src/features/variants/common/types/variants.ts
@@ -1,9 +1,9 @@
 import type { SetupRules } from "@/features/variants/common/types/setupRules";
 import type { PiecesRules } from "@/features/variants/common/types/pieceRules";
 
-type VariantInfo = {
+type VariantRules = {
 	setupRules: SetupRules;
 	piecesRules: PiecesRules;
 };
 
-export type { VariantInfo };
+export type { VariantRules };
