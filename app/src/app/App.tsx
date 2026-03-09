1dc70788fb6fe06e6fd0cf19cd08e34d274e84e1
Teddi <teddikao@gmail.com>
2026-03-09 16:15:28 +0800

    Remove unnecessary cleanup effect

--- app/src/app/App.tsx
+++ app/src/app/App.tsx
@@ -2,18 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 
 import "./global.css";
-import { useEffect } from "react";
-import useVariantsStore from "@/features/variants/common/stores/variantsStore";
 
 function App() {
-	const { resetHydrationState } = useVariantsStore();
-
-	useEffect(() => {
-		return () => {
-			resetHydrationState();
-		};
-	}, [resetHydrationState]);
-
 	return (
 		<BrowserRouter>
 			<Routes>
