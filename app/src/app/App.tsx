6cf5a18c0af8e129400dd1223bbbcddf4c7decb9
Teddi <teddikao@gmail.com>
2026-03-09 12:11:58 +0800

    Reset hydration state on cleanup

--- app/src/app/App.tsx
+++ app/src/app/App.tsx
@@ -1,16 +1,26 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 
 import "./global.css";
+import { useEffect } from "react";
+import useVariantsStore from "@/features/variants/common/stores/variantsStore";
 
 function App() {
+	const { resetHydrationState } = useVariantsStore();
+
+	useEffect(() => {
+		return () => {
+			resetHydrationState();
+		};
+	}, [resetHydrationState]);
+
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<HomePage/>}/>
+				<Route path="/" element={<HomePage />} />
 			</Routes>
 		</BrowserRouter>
 	);
 }
 
-export default App
+export default App;
