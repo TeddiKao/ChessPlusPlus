595d132cb1dd04aa776563bcb3a03706b119a0c0
Teddi <teddikao@gmail.com>
2026-03-12 07:26:15 +0800

    Attach variant delete alert to variant list dialog

--- app/src/features/variants/variantListing/components/VariantListDialog.tsx
+++ app/src/features/variants/variantListing/components/VariantListDialog.tsx
@@ -9,9 +9,10 @@ import {
 import useVariantsStore from "@/features/variants/common/stores/variantsStore";
 import useVariantListDialogStore from "@/features/variants/variantListing/stores/variantListDialogStore";
 import { Button } from "@/components/ui/button";
+import DeleteVariantAlert from "@/features/variants/variantListing/components/DeleteVariantAlert";
 
 function VariantListDialog() {
-	const { variants, hasHydrated } = useVariantsStore();
+	const { variants, hasHydrated, removeVariant } = useVariantsStore();
 	const {
 		isOpen,
 		openDialog,
@@ -32,56 +33,63 @@ function VariantListDialog() {
 	}
 
 	return (
-		<Dialog
-			open={isOpen}
-			onOpenChange={(open) => (open ? openDialog() : closeDialog())}
-		>
-			<DialogContent>
-				<DialogHeader>
-					<DialogTitle>My variants</DialogTitle>
-					<DialogDescription>
-						Below is a list of your created variants
-					</DialogDescription>
-				</DialogHeader>
+		<>
+			<Dialog
+				open={isOpen}
+				onOpenChange={(open) => (open ? openDialog() : closeDialog())}
+			>
+				<DialogContent>
+					<DialogHeader>
+						<DialogTitle>My variants</DialogTitle>
+						<DialogDescription>
+							Below is a list of your created variants
+						</DialogDescription>
+					</DialogHeader>
 
-				<div className="flex flex-col gap-2">
-					{Object.entries(variants).map(
-						([variantId, variantInfo]) => (
-							<button
+					<div className="flex flex-col gap-2">
+						{Object.entries(variants).map(
+							([variantId, variantInfo]) => (
+								<button
+									type="button"
+									onClick={() =>
+										handleVariantSelection(variantId)
+									}
+									className={
+										selectedVariantId === variantId
+											? "flex flex-row gap-2 p-2 rounded-md border-primary border-2 bg-(--muted-primary) [--tw-shadow-color:var(--shadow-primary)] shadow-md"
+											: "flex flex-row gap-2 shadow-md p-2 rounded-md bg-gray-300 shadow-gray-600"
+									}
+									aria-pressed={
+										selectedVariantId === variantId
+									}
+									key={variantId}
+								>
+									<span>{variantInfo.variantName}</span>
+								</button>
+							),
+						)}
+					</div>
+
+					{selectedVariantId && (
+						<DialogFooter>
+							<Button className="px-4" type="button">
+								Rename
+							</Button>
+							<Button
+								className="px-4"
 								type="button"
-								onClick={() =>
-									handleVariantSelection(variantId)
-								}
-								className={
-									selectedVariantId === variantId
-										? "flex flex-row gap-2 p-2 rounded-md border-primary border-2 bg-(--muted-primary) [--tw-shadow-color:var(--shadow-primary)] shadow-md"
-										: "flex flex-row gap-2 shadow-md p-2 rounded-md bg-gray-300 shadow-gray-600"
-								}
-								aria-pressed={selectedVariantId === variantId}
-								key={variantId}
+								variant="destructive"
+								onClick={() => removeVariant(selectedVariantId)}
 							>
-								<span>{variantInfo.variantName}</span>
-							</button>
-						),
+								Delete
+							</Button>
+						</DialogFooter>
 					)}
-				</div>
+				</DialogContent>
+			</Dialog>
 
-				{selectedVariantId && (
-					<DialogFooter>
-						<Button className="px-4" type="button">
-							Rename
-						</Button>
-						<Button
-							className="px-4"
-							type="button"
-							variant="destructive"
-						>
-							Delete
-						</Button>
-					</DialogFooter>
-				)}
-			</DialogContent>
-		</Dialog>
+			<DeleteVariantAlert />
+		</>
 	);
 }
 
