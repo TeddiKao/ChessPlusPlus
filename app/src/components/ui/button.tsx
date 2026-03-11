936c583363246dc5f2cd703d3a184d9fd6f6bf22
Teddi <teddikao@gmail.com>
2026-03-12 07:36:33 +0800

    Prevent accidental form submissions

--- app/src/components/ui/button.tsx
+++ app/src/components/ui/button.tsx
@@ -17,7 +17,7 @@ const buttonVariants = cva(
 					"bg-secondary text-secondary-foreground hover:bg-secondary/80 aria-expanded:bg-secondary aria-expanded:text-secondary-foreground",
 				ghost: "hover:bg-muted hover:text-foreground aria-expanded:bg-muted aria-expanded:text-foreground dark:hover:bg-muted/50",
 				destructive:
-					"bg-destructive text-white hover:bg-destructive/75 focus-visible:border-destructive/40 focus-visible:ring-destructive/20 dark:bg-destructive/20 dark:hover:bg-destructive/30 dark:focus-visible:ring-destructive/40",
+					"bg-destructive text-white hover:bg-destructive/75 focus-visible:border-destructive/40 focus-visible:ring-destructive/20 dark:bg-destructive dark:hover:bg-destructive/75 dark:focus-visible:ring-destructive/40",
 				link: "text-primary underline-offset-4 hover:underline",
 			},
 			size: {
@@ -64,4 +64,4 @@ function Button({
 	);
 }
 
-export { Button, buttonVariants };
+export { Button };
