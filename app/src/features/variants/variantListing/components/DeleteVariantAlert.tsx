be2404a58e3699b1091b887cebe0b3e151ed4827
Teddi <teddikao@gmail.com>
2026-03-12 07:21:50 +0800

    Add markup for delete variant alert

--- app/src/features/variants/variantListing/components/DeleteVariantAlert.tsx
+++ app/src/features/variants/variantListing/components/DeleteVariantAlert.tsx
@@ -1,3 +1,33 @@
-function DeleteVariantAlert() {}
+import {
+	AlertDialog,
+	AlertDialogAction,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+
+function DeleteVariantAlert() {
+	return (
+		<AlertDialog open={true}>
+			<AlertDialogContent>
+				<AlertDialogHeader>
+					<AlertDialogTitle>Delete variant?</AlertDialogTitle>
+					<AlertDialogDescription>
+						Are you sure you want to delete this variant? This
+						action cannot be undone?
+					</AlertDialogDescription>
+				</AlertDialogHeader>
+
+				<AlertDialogFooter>
+					<AlertDialogAction>Delete</AlertDialogAction>
+					<AlertDialogCancel>Cancel</AlertDialogCancel>
+				</AlertDialogFooter>
+			</AlertDialogContent>
+		</AlertDialog>
+	);
+}
 
 export default DeleteVariantAlert;
