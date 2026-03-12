c481a3cd5bab2ca386593fb16bff9145781797e6
Teddi <teddikao@gmail.com>
2026-03-13 07:39:33 +0800

    Create page top bar to navigate back to home page

--- app/src/pages/VariantEditorPage.tsx
+++ app/src/pages/VariantEditorPage.tsx
@@ -1,5 +1,17 @@
+import { IconChevronLeft } from "@tabler/icons-react";
+
 function VariantEditorPage() {
-	return <div>VariantEditorPage</div>;
+	return (
+		<div className="flex-col gap-2 pt-4 pl-4">
+			<div className="flex flex-row gap-2">
+				<button>
+					<IconChevronLeft />
+				</button>
+
+				<p>Variant name</p>
+			</div>
+		</div>
+	);
 }
 
 export default VariantEditorPage;
