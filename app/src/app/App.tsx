6fcb1cb891afbffdfcd7035fd8f54fcc67d4f1e9
Teddi <teddikao@gmail.com>
2026-03-12 17:49:24 +0800

    Add page to react router

--- app/src/app/App.tsx
+++ app/src/app/App.tsx
@@ -2,12 +2,17 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 
 import "./global.css";
+import VariantEditorPage from "@/pages/VariantEditorPage";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<HomePage />} />
+				<Route
+					path="/variant-editor/:variantId"
+					element={<VariantEditorPage />}
+				/>
 			</Routes>
 		</BrowserRouter>
 	);
