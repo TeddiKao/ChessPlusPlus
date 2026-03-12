b0ad3c02078aa2885895175f55cb5f299567a4a5
Teddi <teddikao@gmail.com>
2026-03-12 10:24:48 +0800

    Modify delete alert store to track variantIdToDelete

--- app/src/features/variants/variantListing/stores/variantDeleteAlert.ts
+++ app/src/features/variants/variantListing/stores/variantDeleteAlert.ts
@@ -2,14 +2,24 @@ import { create } from "zustand";
 
 type VariantDeleteAlertStore = {
 	isOpen: boolean;
-	openAlert: () => void;
+	openAlert: (variantId: string) => void;
 	closeAlert: () => void;
+
+	variantIdToDelete: string | null;
+	updateVariantIdToDelete: (variantId: string) => void;
+	clearVariantIdToDelete: () => void;
 };
 
 const useVariantDeleteAlertStore = create<VariantDeleteAlertStore>((set) => ({
 	isOpen: false,
-	openAlert: () => set({ isOpen: true }),
+	openAlert: (variantId) =>
+		set({ isOpen: true, variantIdToDelete: variantId }),
 	closeAlert: () => set({ isOpen: false }),
+
+	variantIdToDelete: null,
+	updateVariantIdToDelete: (variantId) =>
+		set({ variantIdToDelete: variantId }),
+	clearVariantIdToDelete: () => set({ variantIdToDelete: null }),
 }));
 
 export default useVariantDeleteAlertStore;
