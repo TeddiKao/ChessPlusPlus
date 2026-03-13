47d4d04ad0a9286fca8915dce592c8db72de02ac
Teddi <teddikao@gmail.com>
2026-03-13 11:57:51 +0800

    Add TooltipProvider

--- app/src/app/App.tsx
+++ app/src/app/App.tsx
@@ -3,17 +3,20 @@ import HomePage from "../pages/HomePage";
 
 import "./global.css";
 import VariantEditorPage from "@/pages/VariantEditorPage";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 function App() {
 	return (
 		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<HomePage />} />
-				<Route
-					path="/variant-editor/:variantId"
-					element={<VariantEditorPage />}
-				/>
-			</Routes>
+			<TooltipProvider>
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route
+						path="/variant-editor/:variantId"
+						element={<VariantEditorPage />}
+					/>
+				</Routes>
+			</TooltipProvider>
 		</BrowserRouter>
 	);
 }
