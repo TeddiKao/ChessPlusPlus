1d27f150dbda734365a03dc7a65cbbb8e59b8708
Teddi <teddikao@gmail.com>
2026-03-13 17:34:53 +0800

    Remove uneccessary PopoverContent wrapper

--- app/src/features/variants/variantEditor/components/ChessboardSettingsButton.tsx
+++ app/src/features/variants/variantEditor/components/ChessboardSettingsButton.tsx
@@ -4,11 +4,7 @@ import {
 	TooltipTrigger,
 } from "@/components/ui/tooltip";
 import { Grid2X2 } from "lucide-react";
-import {
-	Popover,
-	PopoverContent,
-	PopoverTrigger,
-} from "@/components/ui/popover";
+import { Popover, PopoverTrigger } from "@/components/ui/popover";
 import BoardSettingsMenu from "@/features/variants/variantEditor/components/menus/BoardSettingsMenu";
 
 function ChessboardSettingsButton() {
@@ -22,9 +18,7 @@ function ChessboardSettingsButton() {
 						</button>
 					</PopoverTrigger>
 
-					<PopoverContent>
-						<BoardSettingsMenu />
-					</PopoverContent>
+					<BoardSettingsMenu />
 				</Popover>
 			</TooltipTrigger>
 
