da1db5dbc115b71a34236d095d2a8fd31365d555
Teddi <teddikao@gmail.com>
2026-03-13 21:19:46 +0800

    Attach value and change handers to settings menu inputs

--- app/src/features/variants/variantEditor/components/menus/BoardSettingsMenu.tsx
+++ app/src/features/variants/variantEditor/components/menus/BoardSettingsMenu.tsx
@@ -1,8 +1,21 @@
 import { PopoverContent } from "@/components/ui/popover";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import useBoardSettingsStore from "@/features/variants/variantEditor/stores/boardSettings";
+import type { ChangeEvent } from "react";
 
 function BoardSettingsMenu() {
+	const { boardXSize, boardYSize, updateBoardXSize, updateBoardYSize } =
+		useBoardSettingsStore();
+
+	function handleBoardWidthInputChange(e: ChangeEvent<HTMLInputElement>) {
+		updateBoardXSize(Number(e.target.value));
+	}
+
+	function handleBoardHeightInputChange(e: ChangeEvent<HTMLInputElement>) {
+		updateBoardYSize(Number(e.target.value));
+	}
+
 	return (
 		<PopoverContent align="start" sideOffset={8} side="left">
 			<h2 className="text-base font-semibold">Board settings</h2>
@@ -14,12 +27,22 @@ function BoardSettingsMenu() {
 
 				<div className="grid grid-cols-2 gap-4 items-center">
 					<p>Board width</p>
-					<Input type="number" defaultValue={8} />
+					<Input
+						value={boardXSize}
+						onChange={handleBoardWidthInputChange}
+						type="number"
+						defaultValue={8}
+					/>
 				</div>
 
 				<div className="grid grid-cols-2 gap-4 items-center">
 					<p>Board height</p>
-					<Input type="number" defaultValue={8} />
+					<Input
+						value={boardYSize}
+						onChange={handleBoardHeightInputChange}
+						type="number"
+						defaultValue={8}
+					/>
 				</div>
 			</div>
 		</PopoverContent>
