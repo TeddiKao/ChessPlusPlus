4cc4d3d47ba6fb165b1444945db3cd1cfa9e45aa
Teddi <teddikao@gmail.com>
2026-03-13 17:29:28 +0800

    Create markup for board settings menu

--- app/src/features/variants/variantEditor/components/menus/BoardSettingsMenu.tsx
+++ app/src/features/variants/variantEditor/components/menus/BoardSettingsMenu.tsx
@@ -1,3 +1,29 @@
-function BoardSettingsMenu() {}
+import { PopoverContent } from "@/components/ui/popover";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+function BoardSettingsMenu() {
+	return (
+		<PopoverContent>
+			<h2 className="text-xl">Board settings</h2>
+			<div className="flex flex-col gap-2">
+				<div className="flex flex-row gap-4">
+					<p>Board setup</p>
+					<Button>Edit setup</Button>
+				</div>
+
+				<div className="flex flex-row gap-4">
+					<p>Board width</p>
+					<Input type="number" defaultValue={8} />
+				</div>
+
+				<div className="flex flex-row gap-4">
+					<p>Board height</p>
+					<Input type="number" defaultValue={8} />
+				</div>
+			</div>
+		</PopoverContent>
+	);
+}
 
 export default BoardSettingsMenu;
