209b1ad895986dcfe5c9246014ea05632be5157d
Teddi <teddikao@gmail.com>
2026-03-13 09:09:45 +0800

    Create basic sidebar design

--- app/src/pages/VariantEditorPage.tsx
+++ app/src/pages/VariantEditorPage.tsx
@@ -1,16 +1,28 @@
 import { IconChevronLeft } from "@tabler/icons-react";
+import { ChessKnight, Grid2X2 } from "lucide-react";
 
 function VariantEditorPage() {
 	return (
-		<div className="flex-col gap-2 pt-4 pl-4">
-			<div className="flex flex-row gap-2">
-				<button>
-					<IconChevronLeft />
-				</button>
+		<>
+			<div className="flex-col gap-2 pt-4 pl-4">
+				<div className="flex flex-row gap-2">
+					<button>
+						<IconChevronLeft />
+					</button>
 
-				<p>Variant name</p>
+					<p>Variant name</p>
+				</div>
 			</div>
-		</div>
+
+			<div className="absolute top-0 right-0 flex flex-col gap-4 p-4 h-screen bg-sidebar-primary-foreground">
+				<button type="button">
+					<Grid2X2 strokeWidth={1.5} />
+				</button>
+				<button type="button">
+					<ChessKnight strokeWidth={1.5} />
+				</button>
+			</div>
+		</>
 	);
 }
 
