bdce5c32fe70e6670c4a270579281a95d2aa7794
Teddi <teddikao@gmail.com>
2026-03-13 20:57:25 +0800

    Make popover state controlled with store state

--- app/src/features/variants/variantEditor/components/ChessboardSettingsButton.tsx
+++ app/src/features/variants/variantEditor/components/ChessboardSettingsButton.tsx
@@ -6,14 +6,33 @@ import {
 import { Grid2X2 } from "lucide-react";
 import { Popover, PopoverTrigger } from "@/components/ui/popover";
 import BoardSettingsMenu from "@/features/variants/variantEditor/components/menus/BoardSettingsMenu";
+import useVariantEditorSidebarStore from "@/features/variants/variantEditor/stores/variantEditorSidebarStore";
 
 function ChessboardSettingsButton() {
+	const {
+		currentSelectedSetting,
+		updateCurrentSelectedSetting,
+		clearCurrentSelectedSetting,
+	} = useVariantEditorSidebarStore();
+
 	return (
-		<Popover>
+		<Popover
+			open={currentSelectedSetting === "board"}
+			onOpenChange={(open) =>
+				open
+					? updateCurrentSelectedSetting("board")
+					: clearCurrentSelectedSetting()
+			}
+		>
 			<Tooltip>
 				<TooltipTrigger asChild>
 					<PopoverTrigger asChild>
-						<button type="button">
+						<button
+							onClick={() =>
+								updateCurrentSelectedSetting("board")
+							}
+							type="button"
+						>
 							<Grid2X2 strokeWidth={1.5} />
 						</button>
 					</PopoverTrigger>
