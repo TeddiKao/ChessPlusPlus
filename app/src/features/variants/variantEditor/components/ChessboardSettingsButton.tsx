a7a5a512ca2eec531fb6ae5e6881101d72e62490
Teddi <teddikao@gmail.com>
2026-03-13 17:33:50 +0800

    Make sidebar icon trigger for popover component

--- app/src/features/variants/variantEditor/components/ChessboardSettingsButton.tsx
+++ app/src/features/variants/variantEditor/components/ChessboardSettingsButton.tsx
@@ -4,14 +4,28 @@ import {
 	TooltipTrigger,
 } from "@/components/ui/tooltip";
 import { Grid2X2 } from "lucide-react";
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from "@/components/ui/popover";
+import BoardSettingsMenu from "@/features/variants/variantEditor/components/menus/BoardSettingsMenu";
 
 function ChessboardSettingsButton() {
 	return (
 		<Tooltip>
 			<TooltipTrigger asChild>
-				<button type="button">
-					<Grid2X2 strokeWidth={1.5} />
-				</button>
+				<Popover>
+					<PopoverTrigger asChild>
+						<button type="button">
+							<Grid2X2 strokeWidth={1.5} />
+						</button>
+					</PopoverTrigger>
+
+					<PopoverContent>
+						<BoardSettingsMenu />
+					</PopoverContent>
+				</Popover>
 			</TooltipTrigger>
 
 			<TooltipContent side="left">Board settings</TooltipContent>
