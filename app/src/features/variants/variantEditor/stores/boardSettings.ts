0af13a79c446372421c60ca107cde3f0eca796c0
Teddi <teddikao@gmail.com>
2026-03-13 21:15:37 +0800

    Create store to track inputs for board settings

--- app/src/features/variants/variantEditor/stores/boardSettings.ts
+++ app/src/features/variants/variantEditor/stores/boardSettings.ts
@@ -1,8 +1,9 @@
 import type { SquareInfo } from "@/features/variants/common/types/setupRules";
+import { create } from "zustand";
 
 type BoardSettingsStore = {
 	boardSetup: SquareInfo[];
-	addPieceToBoardSetup: (x: number, y: number) => void;
+	addPieceToBoardSetup: (x: number, y: number, pieceName: string) => void;
 	updateBoardSetup: (newBoardSetup: SquareInfo[]) => void;
 	removePieceFromBoardSetup: (x: number, y: number) => void;
 
@@ -12,3 +13,30 @@ type BoardSettingsStore = {
 	boardYSize: number;
 	updateBoardYSize: (newBoardYSize: number) => void;
 };
+
+const useBoardSettingsStore = create<BoardSettingsStore>((set) => ({
+	boardSetup: [],
+	addPieceToBoardSetup: (x, y, pieceName: string) =>
+		set((state) => ({
+			boardSetup: [...state.boardSetup, { xPos: x, yPos: y, pieceName }],
+		})),
+
+	updateBoardSetup: (newBoardSetup) => set({ boardSetup: newBoardSetup }),
+
+	removePieceFromBoardSetup: (x, y) =>
+		set((state) => {
+			return {
+				boardSetup: state.boardSetup.filter(
+					({ xPos, yPos }) => !(xPos === x && yPos === y),
+				),
+			};
+		}),
+
+	boardXSize: 8,
+	updateBoardXSize: (newBoardXSize) => set({ boardXSize: newBoardXSize }),
+
+	boardYSize: 8,
+	updateBoardYSize: (newBoardYSize) => set({ boardYSize: newBoardYSize }),
+}));
+
+export default useBoardSettingsStore;
