62cd1b4576a0b5d90e125b2e4d639ac773cfe118
Teddi <teddikao@gmail.com>
2026-03-14 21:24:59 +0800

    Create search bar for pieces menu

--- app/src/features/variants/variantEditor/components/menus/PiecesMenu.tsx
+++ app/src/features/variants/variantEditor/components/menus/PiecesMenu.tsx
@@ -1,3 +1,19 @@
-function PiecesMenu() {}
+import { PopoverContent } from "@/components/ui/popover";
+import { Input } from "@/components/ui/input";
+import { InputGroup, InputGroupAddon } from "@/components/ui/input-group";
+import { IconSearch } from "@tabler/icons-react";
+
+function PiecesMenu() {
+	return (
+		<PopoverContent>
+			<InputGroup>
+				<Input type="text" placeholder="Search pieces..." />
+				<InputGroupAddon align="inline-start">
+					<IconSearch />
+				</InputGroupAddon>
+			</InputGroup>
+		</PopoverContent>
+	);
+}
 
 export default PiecesMenu;
