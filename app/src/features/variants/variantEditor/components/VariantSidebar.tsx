2db423d9622165868abb70c2152051c44891756e
Teddi <teddikao@gmail.com>
2026-03-14 21:07:18 +0800

    Update imports accordingly

--- app/src/features/variants/variantEditor/components/VariantSidebar.tsx
+++ app/src/features/variants/variantEditor/components/VariantSidebar.tsx
@@ -1,10 +1,10 @@
-import ChessboardSettingsButton from "@/features/variants/variantEditor/components/ChessboardSettingsButton";
+import BoardSettingsButton from "@/features/variants/variantEditor/components/BoardSettingsButton";
 import PiecesButton from "@/features/variants/variantEditor/components/PiecesButton";
 
 function VariantSidebar() {
 	return (
 		<div className="absolute top-0 right-0 flex flex-col gap-4 p-4 h-screen bg-sidebar-primary-foreground">
-			<ChessboardSettingsButton />
+			<BoardSettingsButton />
 			<PiecesButton />
 		</div>
 	);
