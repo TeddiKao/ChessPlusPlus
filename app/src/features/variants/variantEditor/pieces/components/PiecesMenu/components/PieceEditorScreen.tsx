c7320686481764c6aeb468ea9da21f7ad1039875
Teddi <teddikao@gmail.com>
2026-03-15 13:53:35 +0800

    Add title and description to piece editor screen

--- app/src/features/variants/variantEditor/pieces/components/PiecesMenu/components/PieceEditorScreen.tsx
+++ app/src/features/variants/variantEditor/pieces/components/PiecesMenu/components/PieceEditorScreen.tsx
@@ -1,5 +1,25 @@
+import {
+	SheetDescription,
+	SheetHeader,
+	SheetTitle,
+} from "@/components/ui/sheet";
+import { IconChevronLeft } from "@tabler/icons-react";
+
 function PieceEditorScreen() {
-	return <div>Piece Editor Screen</div>;
+	return (
+		<>
+			<SheetHeader>
+				<SheetTitle className="flex flex-row items-center gap-2">
+					<IconChevronLeft className="size-5" />
+					<span>Piece Editor</span>
+				</SheetTitle>
+				<SheetDescription>
+					You are currently editing the bishop. Click the back arrow
+					to switch your selection.
+				</SheetDescription>
+			</SheetHeader>
+		</>
+	);
 }
 
 export default PieceEditorScreen;
