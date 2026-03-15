a42fe68164eb90d8eec6dd7002b9346eb506e1cb
Teddi <teddikao@gmail.com>
2026-03-15 15:33:47 +0800

    Extract components from piece editor

--- app/src/features/variants/variantEditor/pieces/components/PiecesMenu/components/PieceEditorScreen.tsx
+++ app/src/features/variants/variantEditor/pieces/components/PiecesMenu/components/PieceEditorScreen.tsx
@@ -16,7 +16,105 @@ import {
 	CollapsibleContent,
 	CollapsibleTrigger,
 } from "@/components/ui/collapsible";
-import { Field, FieldLabel } from "@/components/ui/field";
+import {
+	Field,
+	FieldLabel,
+	FieldLegend,
+	FieldSet,
+} from "@/components/ui/field";
+import { Checkbox } from "@/components/ui/checkbox";
+
+function MovementTypeFieldSet() {
+	return (
+		<FieldSet className="gap-2">
+			<FieldLegend variant="label">Movement types</FieldLegend>
+			<Field orientation="horizontal">
+				<Checkbox
+					className="border-gray-600"
+					id="for-movement-checkbox"
+				/>
+				<FieldLabel htmlFor="for-movement-checkbox">
+					For movement
+				</FieldLabel>
+			</Field>
+			<Field orientation="horizontal">
+				<Checkbox
+					className="border-gray-600"
+					id="for-capture-checkbox"
+				/>
+				<FieldLabel htmlFor="for-capture-checkbox">
+					For capture
+				</FieldLabel>
+			</Field>
+		</FieldSet>
+	);
+}
+
+function RangeFieldSet() {
+	return (
+		<FieldSet>
+			<Field
+				className="grid grid-cols-2 gap-2"
+				id="pieceRange"
+				orientation="horizontal"
+			>
+				<FieldLabel htmlFor="pieceRange">Range</FieldLabel>
+				<Input
+					className="bg-background"
+					type="number"
+					placeholder="Range"
+				/>
+			</Field>
+
+			<Field orientation="horizontal">
+				<Checkbox className="border-gray-600" id="infiniteRange" />
+				<FieldLabel htmlFor="infiniteRange">Infinite range</FieldLabel>
+			</Field>
+		</FieldSet>
+	);
+}
+
+function OffsetFieldSet() {
+	return (
+		<FieldSet>
+			<div className="grid grid-cols-2 gap-4 items-center">
+				<FieldLegend
+					className="data-[variant=label]:text-foreground mb-0"
+					variant="label"
+				>
+					Offsets
+				</FieldLegend>
+				<div className="grid grid-cols-2 gap-2">
+					<Field>
+						<Input
+							className="bg-background"
+							type="number"
+							placeholder="X"
+						/>
+					</Field>
+					<Field>
+						<Input
+							className="bg-background"
+							type="number"
+							placeholder="Y"
+						/>
+					</Field>
+				</div>
+			</div>
+		</FieldSet>
+	);
+}
+
+function MoveDefinitionFieldSet() {
+	return (
+		<FieldSet>
+			<FieldLegend variant="label">Move definitions</FieldLegend>
+
+			<OffsetFieldSet />
+			<RangeFieldSet />
+		</FieldSet>
+	);
+}
 
 function PieceEditorScreen() {
 	return (
@@ -81,13 +179,16 @@ function PieceEditorScreen() {
 							className="flex flex-row justify-between w-full"
 							asChild
 						>
-							<Button variant="ghost">
+							<Button className="pb-2" variant="ghost">
 								<span>Movement 1</span>
 								<IconChevronUp />
 							</Button>
 						</CollapsibleTrigger>
 
-						<CollapsibleContent className="flex flex-col gap-4"></CollapsibleContent>
+						<CollapsibleContent className="flex flex-col gap-4">
+							<MovementTypeFieldSet />
+							<MoveDefinitionFieldSet />
+						</CollapsibleContent>
 					</Collapsible>
 				</TabsContent>
 			</Tabs>
