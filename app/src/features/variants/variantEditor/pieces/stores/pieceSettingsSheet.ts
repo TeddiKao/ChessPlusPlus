b11a4b41d947b1097327b39e49c1fea50c6449e2
Teddi <teddikao@gmail.com>
2026-03-15 11:53:45 +0800

    Add state to settings sheet store to keep track of sheet mode

--- app/src/features/variants/variantEditor/pieces/stores/pieceSettingsSheet.ts
+++ app/src/features/variants/variantEditor/pieces/stores/pieceSettingsSheet.ts
@@ -4,12 +4,20 @@ type PieceSettingsStore = {
 	isOpen: boolean;
 	openPieceSettingsSheet: () => void;
 	closePieceSettingsSheet: () => void;
+
+	currentSheetMode: "pieceSelection" | "pieceConfiguration";
+	updateCurrentSheetMode: (
+		mode: "pieceSelection" | "pieceConfiguration",
+	) => void;
 };
 
 const usePieceSettingsStore = create<PieceSettingsStore>((set) => ({
 	isOpen: false,
 	openPieceSettingsSheet: () => set({ isOpen: true }),
 	closePieceSettingsSheet: () => set({ isOpen: false }),
+
+	currentSheetMode: "pieceSelection",
+	updateCurrentSheetMode: (mode) => set({ currentSheetMode: mode }),
 }));
 
 export default usePieceSettingsStore;
