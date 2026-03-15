2e10e50188a7e6d6ec823c1dabada6db4b8dbb3e
Teddi <teddikao@gmail.com>
2026-03-15 10:30:20 +0800

    Update button structure to remove popover

--- app/src/features/variants/variantEditor/components/PiecesButton.tsx
+++ app/src/features/variants/variantEditor/components/PiecesButton.tsx
@@ -4,18 +4,23 @@ import {
 	TooltipTrigger,
 } from "@/components/ui/tooltip";
 import { ChessKnight } from "lucide-react";
+import PiecesMenu from "@/features/variants/variantEditor/components/menus/PiecesMenu";
 
 function PiecesButton() {
 	return (
-		<Tooltip>
-			<TooltipTrigger asChild>
-				<button type="button">
-					<ChessKnight strokeWidth={1.5} />
-				</button>
-			</TooltipTrigger>
+		<>
+			<Tooltip>
+				<TooltipTrigger asChild>
+					<button type="button">
+						<ChessKnight strokeWidth={1.5} />
+					</button>
+				</TooltipTrigger>
 
-			<TooltipContent side="left">Pieces</TooltipContent>
-		</Tooltip>
+				<TooltipContent side="left">Pieces</TooltipContent>
+			</Tooltip>
+
+			<PiecesMenu />
+		</>
 	);
 }
 
