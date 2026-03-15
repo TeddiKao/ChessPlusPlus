ee11a358e6dcad0df5a76de6117136d0def915a5
Teddi <teddikao@gmail.com>
2026-03-15 13:42:55 +0800

    Use piece editor screeen when current sheet mode corresponds to it

--- app/src/features/variants/variantEditor/pieces/components/PiecesMenu/PiecesMenu.tsx
+++ app/src/features/variants/variantEditor/pieces/components/PiecesMenu/PiecesMenu.tsx
@@ -1,6 +1,7 @@
 import { Sheet, SheetContent } from "@/components/ui/sheet";
 import usePieceSettingsStore from "@/features/variants/variantEditor/pieces/stores/pieceSettingsSheet";
 import PiecesSelectionScreen from "@/features/variants/variantEditor/pieces/components/PiecesMenu/components/PiecesSelectionScreen";
+import PieceEditorScreen from "@/features/variants/variantEditor/pieces/components/PiecesMenu/components/PieceEditorScreen";
 
 function PiecesMenu() {
 	const {
@@ -23,7 +24,9 @@ function PiecesMenu() {
 			>
 				{currentSheetMode === "pieceSelection" ? (
 					<PiecesSelectionScreen />
-				) : null}
+				) : (
+					<PieceEditorScreen />
+				)}
 			</SheetContent>
 		</Sheet>
 	);
