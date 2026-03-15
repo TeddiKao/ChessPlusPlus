a8e2bbb3bf43cc75ce86b763d5f004ac413232c6
Teddi <teddikao@gmail.com>
2026-03-15 11:35:41 +0800

    Remove expanded state from store

--- app/src/features/variants/variantEditor/stores/pieceSettings/pieceSettingsSheet.ts
+++ app/src/features/variants/variantEditor/stores/pieceSettings/pieceSettingsSheet.ts
@@ -4,28 +4,12 @@ type PieceSettingsStore = {
 	isOpen: boolean;
 	openPieceSettingsSheet: () => void;
 	closePieceSettingsSheet: () => void;
-
-	isDefaultPiecesExpanded: boolean;
-	expandDefaultPieces: () => void;
-	collapseDefaultPieces: () => void;
-
-	isCustomPiecesExpanded: boolean;
-	expandCustomPieces: () => void;
-	collapseCustomPieces: () => void;
 };
 
 const usePieceSettingsStore = create<PieceSettingsStore>((set) => ({
 	isOpen: false,
 	openPieceSettingsSheet: () => set({ isOpen: true }),
 	closePieceSettingsSheet: () => set({ isOpen: false }),
-
-	isDefaultPiecesExpanded: false,
-	expandDefaultPieces: () => set({ isDefaultPiecesExpanded: true }),
-	collapseDefaultPieces: () => set({ isDefaultPiecesExpanded: false }),
-
-	isCustomPiecesExpanded: false,
-	expandCustomPieces: () => set({ isCustomPiecesExpanded: true }),
-	collapseCustomPieces: () => set({ isCustomPiecesExpanded: false }),
 }));
 
 export default usePieceSettingsStore;
