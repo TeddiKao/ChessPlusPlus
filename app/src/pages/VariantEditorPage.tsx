c480174b76f9faeb0509e91e9d911f09e446a6c5
Teddi <teddikao@gmail.com>
2026-03-16 14:23:27 +0800

    Use shadcn ghost variant button

--- app/src/pages/VariantEditorPage.tsx
+++ app/src/pages/VariantEditorPage.tsx
@@ -1,14 +1,15 @@
 import { IconChevronLeft } from "@tabler/icons-react";
 import VariantSidebar from "@/features/variants/variantEditor/common/components/VariantSidebar";
+import { Button } from "@/components/ui/button";
 
 function VariantEditorPage() {
 	return (
 		<>
 			<div className="flex-col gap-2 pt-4 pl-4">
-				<div className="flex flex-row gap-2">
-					<button>
-						<IconChevronLeft />
-					</button>
+				<div className="flex flex-row gap-2 items-center">
+					<Button variant="ghost">
+						<IconChevronLeft className="size-5" />
+					</Button>
 
 					<p>Variant name</p>
 				</div>
