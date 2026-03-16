6735edbda5aa9b600ec8e50568c29d4902a6f735
Teddi <teddikao@gmail.com>
2026-03-16 14:24:31 +0800

    Add navigation back to home page functionality

--- app/src/pages/VariantEditorPage.tsx
+++ app/src/pages/VariantEditorPage.tsx
@@ -1,13 +1,16 @@
 import { IconChevronLeft } from "@tabler/icons-react";
 import VariantSidebar from "@/features/variants/variantEditor/common/components/VariantSidebar";
 import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
 
 function VariantEditorPage() {
+	const navigate = useNavigate();
+
 	return (
 		<>
 			<div className="flex-col gap-2 pt-4 pl-4">
 				<div className="flex flex-row gap-2 items-center">
-					<Button variant="ghost">
+					<Button onClick={() => navigate("/")} variant="ghost">
 						<IconChevronLeft className="size-5" />
 					</Button>
 
