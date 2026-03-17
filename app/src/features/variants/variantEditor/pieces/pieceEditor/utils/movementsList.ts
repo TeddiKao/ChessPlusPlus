9e92eba76896ae714a25257bd59f210bd5b09bf2
Teddi <teddikao@gmail.com>
2026-03-17 20:28:23 +0800

    Create utility functions to get list of movements and paths given a piece's rules

--- app/src/features/variants/variantEditor/pieces/pieceEditor/utils/movementsList.ts
+++ app/src/features/variants/variantEditor/pieces/pieceEditor/utils/movementsList.ts
@@ -1,17 +1,49 @@
-import type { PiecesRules } from "@/features/variants/common/types/pieceRules";
+import type {
+	ChainedMovePath,
+	MoveNode,
+	PieceRules,
+} from "@/features/variants/common/types/pieceRules";
 
-function getMovementsListForPiece(pieces: PiecesRules, pieceName: string) {
-	if (!pieces[pieceName]) return [];
+function loopOverChainedMoves(
+	chainedMoves: MoveNode[],
+	movementsList: { moveName: string; path: ChainedMovePath }[],
+	movePath: number[],
+) {
+	for (const movementInfo of chainedMoves) {
+		const index = chainedMoves.findIndex(
+			(move) => move.moveName === movementInfo.moveName,
+		);
 
-	const movementsList: string[] = [];
-	for (const movementInfo of pieces[pieceName].moves) {
-		movementsList.push(movementInfo.moveName);
+		const updatedMovePath = [...movePath, index];
 
-		let currentMoveNode = movementInfo;
-		while (currentMoveNode.chainedMoves.length > 0) {
-			movementsList.push(currentMoveNode.chainedMoves[0].moveName);
-			currentMoveNode = currentMoveNode.chainedMoves[0];
-		}
+		movementsList.push({
+			moveName: movementInfo.moveName,
+			path: updatedMovePath,
+		});
+
+		loopOverChainedMoves(
+			movementInfo.chainedMoves,
+			movementsList,
+			updatedMovePath,
+		);
+	}
+}
+
+function getMovementsListForPiece(pieceRules: PieceRules) {
+	const movementsList: { moveName: string; path: ChainedMovePath }[] = [];
+
+	for (const movementInfo of pieceRules.moves) {
+		const rootIndex = pieceRules.moves.findIndex(
+			(move) => move.moveName === movementInfo.moveName,
+		);
+		const movePath: number[] = [rootIndex];
+
+		let chainedMoves: MoveNode[] | null = null;
+
+		movementsList.push({ moveName: movementInfo.moveName, path: movePath });
+		chainedMoves = movementInfo.chainedMoves;
+
+		loopOverChainedMoves(chainedMoves, movementsList, movePath);
 	}
 
 	return movementsList;
