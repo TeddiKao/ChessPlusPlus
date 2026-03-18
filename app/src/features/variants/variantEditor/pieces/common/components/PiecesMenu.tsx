875787e8a94c2a60ce4e03f9242aa1ae078547af
Teddi <teddikao@gmail.com>
2026-03-18 10:30:11 +0800

    Update directory structure to prepare refactoring

--- app/src/features/variants/variantEditor/pieces/common/components/PiecesMenu.tsx
+++ app/src/features/variants/variantEditor/pieces/common/components/PiecesMenu.tsx
@@ -1,7 +1,7 @@
 import { Sheet, SheetContent } from "@/components/ui/sheet";
 import usePieceSettingsStore from "@/features/variants/variantEditor/pieces/common/stores/pieceSettingsSheet";
 import PiecesSelectionScreen from "@/features/variants/variantEditor/pieces/pieceSelection/components/PiecesSelectionScreen";
-import PieceEditorScreen from "@/features/variants/variantEditor/pieces/pieceEditor/components/PieceEditorScreen";
+import PieceEditorScreen from "@/features/variants/variantEditor/pieces/pieceEditor/components/PieceEditorScreen/PieceEditorScreen";
 import useVariantEditorSidebarStore from "@/features/variants/variantEditor/common/stores/variantEditorSidebar";
 
 function PiecesMenu() {
