a1001638bea90af2fb1163dc1ad5f43fa0f9d332
Teddi <teddikao@gmail.com>
2026-03-18 10:42:19 +0800

    Move AppearanceTab component to separate file

---
+++ app/src/features/variants/variantEditor/pieces/pieceEditor/components/PieceEditorScreen/components/AppearanceTab.tsx
@@ -0,0 +1,38 @@
+import { TabsContent } from "@/components/ui/tabs";
+import { Field, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { IconUpload } from "@tabler/icons-react";
+
+export function AppearanceTab() {
+	return (
+		<TabsContent value="appearance" className="flex flex-col gap-4 w-full">
+			<Field className="grid grid-cols-2 gap-4 items-center">
+				<FieldLabel className="font-normal" htmlFor="pieceName">
+					Piece name
+				</FieldLabel>
+				<Input
+					id="pieceName"
+					className="bg-white"
+					placeholder="Piece name"
+				/>
+			</Field>
+
+			<div className="grid grid-cols-2 gap-4 items-center">
+				<p>Piece image (white)</p>
+				<Button className="flex flex-row gap-2" variant="outline">
+					<IconUpload />
+					<span>Upload image</span>
+				</Button>
+			</div>
+
+			<div className="grid grid-cols-2 gap-4 items-center">
+				<p>Piece image (black)</p>
+				<Button className="flex flex-row gap-2" variant="outline">
+					<IconUpload />
+					<span>Upload image</span>
+				</Button>
+			</div>
+		</TabsContent>
+	);
+}
