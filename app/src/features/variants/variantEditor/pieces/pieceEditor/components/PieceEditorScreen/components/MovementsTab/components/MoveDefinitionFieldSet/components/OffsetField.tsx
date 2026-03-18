a3b07bfd1b1fe95f25c56bdd93c4c3fe8c846777
Teddi <teddikao@gmail.com>
2026-03-18 12:37:05 +0800

    Add value and onChange handlers to offset inputs

--- app/src/features/variants/variantEditor/pieces/pieceEditor/components/PieceEditorScreen/components/MovementsTab/components/MoveDefinitionFieldSet/components/OffsetField.tsx
+++ app/src/features/variants/variantEditor/pieces/pieceEditor/components/PieceEditorScreen/components/MovementsTab/components/MoveDefinitionFieldSet/components/OffsetField.tsx
@@ -1,7 +1,20 @@
 import { Field, FieldLabel } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
+import usePieceMovementEditorStore from "@/features/variants/variantEditor/pieces/pieceEditor/stores/pieceMovementEditor";
+import type { ChangeEvent } from "react";
 
 function OffsetField() {
+	const { offsetX, updateOffsetX, offsetY, updateOffsetY } =
+		usePieceMovementEditorStore();
+
+	function handleOffsetXChange(event: ChangeEvent<HTMLInputElement>) {
+		updateOffsetX(Number(event.target.value));
+	}
+
+	function handleOffsetYChange(event: ChangeEvent<HTMLInputElement>) {
+		updateOffsetY(Number(event.target.value));
+	}
+
 	return (
 		<Field className="grid grid-cols-2 gap-2">
 			<FieldLabel className="text-foreground mb-0 font-normal">
@@ -13,11 +26,15 @@ function OffsetField() {
 					className="bg-background"
 					type="number"
 					placeholder="X"
+					value={offsetX}
+					onChange={handleOffsetXChange}
 				/>
 				<Input
 					className="bg-background"
 					type="number"
 					placeholder="Y"
+					value={offsetY}
+					onChange={handleOffsetYChange}
 				/>
 			</div>
 		</Field>
