6da920fdf763f56c164854661c7fd1070430e953
Teddi <teddikao@gmail.com>
2026-03-19 11:15:34 +0800

    Add state to track changes in appearance editor

--- app/src/features/variants/variantEditor/pieces/pieceEditor/stores/appearanceEditor.ts
+++ app/src/features/variants/variantEditor/pieces/pieceEditor/stores/appearanceEditor.ts
@@ -1,5 +1,11 @@
 import { create } from "zustand";
 
+type AppearanceEditorChanges = {
+	pieceName: string;
+	whitePieceImage: Blob | null;
+	blackPieceImage: Blob | null;
+};
+
 type AppearanceEditorStore = {
 	pieceName: string | null;
 	updatePieceName: (newPieceName: string) => void;
@@ -12,6 +18,15 @@ type AppearanceEditorStore = {
 	blackPieceImage: Blob | null;
 	updateBlackPieceImage: (newImage: Blob) => void;
 	clearBlackPieceImage: () => void;
+
+	appearanceEditorChanges: Partial<AppearanceEditorChanges>;
+	addAppearanceEditorChanges: (
+		changes: Partial<AppearanceEditorChanges>,
+	) => void;
+	removeAppearanceEditorChanges: (
+		keys: (keyof AppearanceEditorChanges)[],
+	) => void;
+	clearAppearanceEditorChanges: () => void;
 };
 
 const useAppearanceEditorStore = create<AppearanceEditorStore>((set) => ({
@@ -26,6 +41,29 @@ const useAppearanceEditorStore = create<AppearanceEditorStore>((set) => ({
 	blackPieceImage: null,
 	updateBlackPieceImage: (newImage) => set({ blackPieceImage: newImage }),
 	clearBlackPieceImage: () => set({ blackPieceImage: null }),
+
+	appearanceEditorChanges: {},
+	addAppearanceEditorChanges: (changes) =>
+		set((state) => ({
+			appearanceEditorChanges: {
+				...state.appearanceEditorChanges,
+				...changes,
+			},
+		})),
+
+	removeAppearanceEditorChanges: (keys) => {
+		set((state) => {
+			const newState = structuredClone(state);
+
+			for (const key of keys) {
+				delete newState.appearanceEditorChanges[key];
+			}
+
+			return newState;
+		});
+	},
+
+	clearAppearanceEditorChanges: () => set({ appearanceEditorChanges: {} }),
 }));
 
 export default useAppearanceEditorStore;
