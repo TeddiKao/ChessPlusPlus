57524cb06d1cffd9f1191e6c7b1b6a65b102c6e4
Teddi <teddikao@gmail.com>
2026-03-23 15:32:22 +0800

    Add state to track changes to setup settings editor to later commit to draft

--- app/src/features/variants/variantEditor/stores/setupSettingsEditor.ts
+++ app/src/features/variants/variantEditor/stores/setupSettingsEditor.ts
@@ -1,5 +1,10 @@
 import { create } from "zustand";
 
+type SetupSettingsChanges = {
+	boardXSize: number;
+	boardYSize: number;
+};
+
 type SetupSettingsEditorStore = {
 	boardXSize: number | null;
 	updateBoardXSize: (newSize: number) => void;
@@ -8,6 +13,13 @@ type SetupSettingsEditorStore = {
 	boardYSize: number | null;
 	updateBoardYSize: (newSize: number) => void;
 	clearBoardYSize: () => void;
+
+	setupSettingsChanges: Partial<SetupSettingsChanges>;
+	addSetupSettingsChanges: (changes: Partial<SetupSettingsChanges>) => void;
+	removeSetupSettingsChanges: (
+		changeKeys: (keyof SetupSettingsChanges)[],
+	) => void;
+	clearSetupSettingsChanges: () => void;
 };
 
 const useSetupSettingsEditorStore = create<SetupSettingsEditorStore>((set) => ({
@@ -18,6 +30,28 @@ const useSetupSettingsEditorStore = create<SetupSettingsEditorStore>((set) => ({
 	boardYSize: null,
 	updateBoardYSize: (newSize) => set({ boardYSize: newSize }),
 	clearBoardYSize: () => set({ boardYSize: null }),
+
+	setupSettingsChanges: {},
+	addSetupSettingsChanges: (changes) =>
+		set((state) => ({
+			setupSettingsChanges: {
+				...state.setupSettingsChanges,
+				...changes,
+			},
+		})),
+	removeSetupSettingsChanges: (changeKeys) => {
+		set((state) => {
+			const newChanges = structuredClone(state.setupSettingsChanges);
+
+			for (const key of changeKeys) {
+				delete newChanges[key];
+			}
+
+			return { setupSettingsChanges: newChanges };
+		});
+	},
+
+	clearSetupSettingsChanges: () => set({ setupSettingsChanges: {} }),
 }));
 
 export default useSetupSettingsEditorStore;
