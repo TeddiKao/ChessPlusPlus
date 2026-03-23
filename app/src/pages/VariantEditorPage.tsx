06cd984060d1b93076011b48f7ae88e4946bcde1
Teddi <teddikao@gmail.com>
2026-03-23 09:49:44 +0800

    Create design for page header

--- app/src/pages/VariantEditorPage.tsx
+++ app/src/pages/VariantEditorPage.tsx
@@ -1,5 +1,24 @@
+import { IconChevronLeft } from "@tabler/icons-react";
+import { Button } from "@/components/ui/button";
+import { useParams } from "react-router-dom";
+import useVariantsStore from "@/features/variants/common/stores/variantsStore";
+
 function VariantEditorPage() {
-	return <></>;
+	const { variantId } = useParams();
+	const { variants } = useVariantsStore();
+
+	if (!variantId) return null;
+
+	const variantName = variants[variantId].variantName;
+
+	return (
+		<div className="flex flex-col gap-2">
+			<Button variant="ghost" className="flex flex-row gap-2">
+				<IconChevronLeft className="size-6" />
+				<span>{variantName}</span>
+			</Button>
+		</div>
+	);
 }
 
 export default VariantEditorPage;
