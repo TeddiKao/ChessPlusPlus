123deef74188b456392fb650c2e13c7b8af38099
Teddi <teddikao@gmail.com>
2026-03-23 15:32:13 +0800

    Add state to track changes to pieces editor to later commit to draft

--- app/src/features/variants/variantEditor/stores/piecesEditor.ts
+++ app/src/features/variants/variantEditor/stores/piecesEditor.ts
@@ -1,9 +1,20 @@
 import { create } from "zustand";
 
+type PieceEditorChanges = {
+	activePieceMovements: string[];
+};
+
 type PiecesEditorStore = {
 	activePiece: string | null;
 	updateActivePiece: (newActivePiece: string) => void;
 	clearActivePiece: () => void;
+
+	piecesEditorChanges: Partial<PieceEditorChanges>;
+	addPieceEditorChanges: (changes: Partial<PieceEditorChanges>) => void;
+	removePieceEditorChanges: (
+		changeKeys: (keyof PieceEditorChanges)[],
+	) => void;
+	clearPieceEditorChanges: () => void;
 
 	activePieceMovements: string[];
 	addMovementToActivePiece: (movementName: string) => void;
@@ -15,6 +26,28 @@ const usePiecesEditorStore = create<PiecesEditorStore>((set) => ({
 	activePiece: null,
 	updateActivePiece: (newPiece) => set({ activePiece: newPiece }),
 	clearActivePiece: () => set({ activePiece: null }),
+
+	piecesEditorChanges: {},
+	addPieceEditorChanges: (changes) =>
+		set((state) => ({
+			piecesEditorChanges: {
+				...state.piecesEditorChanges,
+				...changes,
+			},
+		})),
+
+	removePieceEditorChanges: (changes) => {
+		set((state) => {
+			const newChanges = structuredClone(state.piecesEditorChanges);
+			for (const key of changes) {
+				delete newChanges[key];
+			}
+
+			return { piecesEditorChanges: newChanges };
+		});
+	},
+
+	clearPieceEditorChanges: () => set({ piecesEditorChanges: {} }),
 
 	activePieceMovements: [],
 	addMovementToActivePiece: (movementName) =>
