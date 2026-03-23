f2c66a21f3fa34a965bb264158616c14ff66af78
Teddi <teddikao@gmail.com>
2026-03-23 10:10:16 +0800

    Reduce size of button

--- app/src/pages/VariantEditorPage.tsx
+++ app/src/pages/VariantEditorPage.tsx
@@ -13,10 +13,13 @@ function VariantEditorPage() {
 
 	return (
 		<div className="flex flex-col gap-2">
-			<Button variant="ghost" className="flex flex-row gap-2">
-				<IconChevronLeft className="size-6" />
+			<div className="flex flex-row px-4 py-4 items-center">
+				<Button size="xs" data-icon="inline-start" variant="ghost">
+					<IconChevronLeft className="size-5" />
+				</Button>
+
 				<span>{variantName}</span>
-			</Button>
+			</div>
 		</div>
 	);
 }
