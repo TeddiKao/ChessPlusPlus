f4f300bfca4b5f24e03d2dc308f926635a892713
Teddi <teddikao@gmail.com>
2026-03-23 21:06:05 +0800

    Update movesets to match new chained moves structure

--- app/src/features/variants/variantCreation/constants/pieceRuleDefaults.ts
+++ app/src/features/variants/variantCreation/constants/pieceRuleDefaults.ts
@@ -7,35 +7,27 @@ const queenRules: PieceRules = {
 	moveset: [
 		{
 			moveName: "north",
-			chainedMoves: [],
 		},
 		{
 			moveName: "south",
-			chainedMoves: [],
 		},
 		{
 			moveName: "east",
-			chainedMoves: [],
 		},
 		{
 			moveName: "west",
-			chainedMoves: [],
 		},
 		{
 			moveName: "northeast",
-			chainedMoves: [],
 		},
 		{
 			moveName: "northwest",
-			chainedMoves: [],
 		},
 		{
 			moveName: "southeast",
-			chainedMoves: [],
 		},
 		{
 			moveName: "southwest",
-			chainedMoves: [],
 		},
 	],
 };
@@ -44,19 +36,15 @@ const rookRules: PieceRules = {
 	moveset: [
 		{
 			moveName: "north",
-			chainedMoves: [],
 		},
 		{
 			moveName: "south",
-			chainedMoves: [],
 		},
 		{
 			moveName: "east",
-			chainedMoves: [],
 		},
 		{
 			moveName: "west",
-			chainedMoves: [],
 		},
 	],
 };
@@ -64,19 +52,15 @@ const bishopRules: PieceRules = {
 	moveset: [
 		{
 			moveName: "northeast",
-			chainedMoves: [],
 		},
 		{
 			moveName: "northwest",
-			chainedMoves: [],
 		},
 		{
 			moveName: "southeast",
-			chainedMoves: [],
 		},
 		{
 			moveName: "southwest",
-			chainedMoves: [],
 		},
 	],
 };
@@ -84,36 +68,28 @@ const knightRules: PieceRules = {
 	moveset: [
 		{
 			moveName: "knight_0201",
-			chainedMoves: [],
 		},
 		{
 			moveName: "knight_02m1",
-			chainedMoves: [],
 		},
 		{
 			moveName: "knight_m201",
-			chainedMoves: [],
 		},
 		{
 			moveName: "knight_m2m1",
-			chainedMoves: [],
 		},
 		{
 			moveName: "knight_0102",
-			chainedMoves: [],
 		},
 		{
 			moveName: "knight_m102",
-			chainedMoves: [],
 		},
 		{
 			moveName: "knight_01m2",
-			chainedMoves: [],
 		},
 		{
 			moveName: "knight_m1m2",
-			chainedMoves: [],
-		}
+		},
 	],
 };
 
@@ -121,35 +97,27 @@ const kingRules: PieceRules = {
 	moveset: [
 		{
 			moveName: "north_1",
-			chainedMoves: [],
 		},
 		{
 			moveName: "south_1",
-			chainedMoves: [],
 		},
 		{
 			moveName: "east_1",
-			chainedMoves: [],
 		},
 		{
 			moveName: "west_1",
-			chainedMoves: [],
 		},
 		{
 			moveName: "northeast_1",
-			chainedMoves: [],
 		},
 		{
 			moveName: "northwest_1",
-			chainedMoves: [],
 		},
 		{
 			moveName: "southeast_1",
-			chainedMoves: [],
 		},
 		{
 			moveName: "southwest_1",
-			chainedMoves: [],
 		},
 	],
 };
@@ -157,39 +125,31 @@ const kingRules: PieceRules = {
 const defaultPieceRuleset: PieceRuleset = {
 	whitePawn: {
 		moveset: [
-			{
-				moveName: "white_pawn_forward",
-				chainedMoves: [
-					["white_pawn_double_step"]
-				],
-			},
+			[
+				{ moveName: "white_pawn_forward", validMove: true },
+				{ moveName: "white_pawn_double_step", validMove: true },
+			],
 			{
 				moveName: "white_pawn_capture_east",
-				chainedMoves: [],
 			},
 			{
 				moveName: "white_pawn_capture_west",
-				chainedMoves: [],
-			}
-		]
+			},
+		],
 	},
 
 	blackPawn: {
 		moveset: [
-			{
-				moveName: "black_pawn_forward",
-				chainedMoves: [
-					["black_forward_double_step"]
-				],
-			},
+			[
+				{ moveName: "black_pawn_forward", validMove: true },
+				{ moveName: "black_forward_double_step", validMove: true },
+			],
 			{
 				moveName: "black_pawn_capture_east",
-				chainedMoves: [],
 			},
 			{
 				moveName: "black_pawn_capture_west",
-				chainedMoves: [],
-			}
+			},
 		],
 	},
 
