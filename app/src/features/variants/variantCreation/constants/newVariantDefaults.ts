9a575675e99c3f12c5c776fe01fc5c4dde81287e
Teddi <teddikao@gmail.com>
2026-03-23 11:34:27 +0800

    Add movement rules to top level default variant rules

--- app/src/features/variants/variantCreation/constants/newVariantDefaults.ts
+++ app/src/features/variants/variantCreation/constants/newVariantDefaults.ts
@@ -1,10 +1,12 @@
 import type { VariantRules } from "@/features/variants/common/types/variants";
-import { defaultPieceRules } from "@/features/variants/variantCreation/constants/pieceRuleDefaults";
+import { defaultPieceRuleset } from "@/features/variants/variantCreation/constants/pieceRuleDefaults";
 import { defaultSetupRules } from "@/features/variants/variantCreation/constants/defaultSetupRules";
+import { defaultMovementRules } from "@/features/variants/variantCreation/constants/defaultMovementRules";
 
 const defaultVariantRules: VariantRules = {
 	setupRules: defaultSetupRules,
-	piecesRules: defaultPieceRules,
+	pieceRuleset: defaultPieceRuleset,
+	movementRules: defaultMovementRules
 };
 
 export { defaultVariantRules };
