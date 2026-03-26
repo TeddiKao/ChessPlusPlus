82b2a60c8057b84cc25e53bffa4cb0e48643ccca
Teddi <teddikao@gmail.com>
2026-03-26 10:31:52 +0800

    Create service function to validate JSON

--- app/src/features/test/services/jsonValidatorTest.ts
+++ app/src/features/test/services/jsonValidatorTest.ts
@@ -1,3 +1,11 @@
-function validateJSON() {}
+import api from "@/app/api";
+
+async function validateJSON(json: Record<string, unknown>) {
+	const response = await api.post("/json-validator-test", {
+		jsonToValidate: json,
+	});
+
+	return response.data;
+}
 
 export { validateJSON };
