91c3eafd906ea06298b9605f78b27958142c67d8
Teddi <teddikao@gmail.com>
2026-03-26 13:06:49 +0800

    Add API endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from pydantic_settings.sources import PydanticModel
 from starlette.middleware.cors import CORSMiddleware
+from app.engine.json_validator import validate_json
+from app.schemas.base_schema import BaseSchema
 
 app = FastAPI()
 
@@ -12,6 +16,18 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+class JSONValidationRequest(BaseSchema):
+    json_to_validate: dict
+
 @app.post("/test")
 async def test_route():
     return {"message": "Hello, World!"}
+
+@app.post("/json-validator-test")
+async def json_validator_test(validation_request: JSONValidationRequest):
+    try:
+        validation_status = validate_json(validation_request.json_to_validate)
+
+        return {"validation_status": validation_status}
+    except Exception as e:
+        print(str(e))
