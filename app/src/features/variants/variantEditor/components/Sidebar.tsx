c00b7f581c3b804895f4b6108cdd91bfa455a204
Teddi <teddikao@gmail.com>
2026-03-27 07:47:51 +0800

    Add hover effects to buttons

--- app/src/features/variants/variantEditor/components/Sidebar.tsx
+++ app/src/features/variants/variantEditor/components/Sidebar.tsx
@@ -2,36 +2,55 @@ import ChessboardGridIcon from "@/features/variants/icons/ChessboardGridIcon";
 import { ChessKnightIcon } from "lucide-react";
 import { IconArrowsMove } from "@tabler/icons-react";
 import { Button } from "@/components/ui/button";
-import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 function Sidebar() {
 	return (
 		<div className="flex flex-col gap-4 items-center absolute right-0 top-0 h-screen bg-sidebar-primary-foreground p-3">
 			<Tooltip>
 				<TooltipTrigger asChild>
-					<Button variant="ghost" className="p-1">
+					<Button
+						variant="ghost"
+						className="p-1 hover:bg-(--sidebar-primary-hover)"
+					>
 						<ChessboardGridIcon className="size-5" />
 					</Button>
 				</TooltipTrigger>
-				<TooltipContent side="left" sideOffset={8}>Board settings</TooltipContent>
+				<TooltipContent side="left" sideOffset={8}>
+					Board settings
+				</TooltipContent>
 			</Tooltip>
 
 			<Tooltip>
 				<TooltipTrigger asChild>
-					<Button variant="ghost" className="p-1">
+					<Button
+						variant="ghost"
+						className="p-1 hover:bg-(--sidebar-primary-hover)"
+					>
 						<ChessKnightIcon strokeWidth={1.5} className="size-6" />
 					</Button>
 				</TooltipTrigger>
-				<TooltipContent side="left" sideOffset={8}>Pieces</TooltipContent>
+				<TooltipContent side="left" sideOffset={8}>
+					Pieces
+				</TooltipContent>
 			</Tooltip>
 
 			<Tooltip>
 				<TooltipTrigger asChild>
-					<Button variant="ghost" className="p-1">
+					<Button
+						variant="ghost"
+						className="p-1 hover:bg-[var(--sidebar-primary-hover)]"
+					>
 						<IconArrowsMove strokeWidth={1.5} className="size-6" />
 					</Button>
 				</TooltipTrigger>
-				<TooltipContent side="left" sideOffset={8}>Movements</TooltipContent>
+				<TooltipContent side="left" sideOffset={8}>
+					Movements
+				</TooltipContent>
 			</Tooltip>
 		</div>
 	);
