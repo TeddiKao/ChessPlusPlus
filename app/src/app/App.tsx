e38dc5876543c0659b42a3f0bdc9073dc15889e6
Teddi <teddikao@gmail.com>
2026-03-26 21:11:50 +0800

    Add tooltip provider to app component

--- app/src/app/App.tsx
+++ app/src/app/App.tsx
@@ -5,23 +5,26 @@ import "./global.css";
 import TestPage from "@/pages/TestPage";
 import JSONValidatorTestPage from "@/pages/JSONValidatorTestPage";
 import VariantEditorPage from "@/pages/VariantEditorPage";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 function App() {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<HomePage />} />
-				<Route
-					path="/variants/:variantId"
-					element={<VariantEditorPage />}
-				/>
-				<Route path="/test" element={<TestPage />} />
-				<Route
-					path="/json-validator-test"
-					element={<JSONValidatorTestPage />}
-				/>
-			</Routes>
-		</BrowserRouter>
+		<TooltipProvider>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route
+						path="/variants/:variantId"
+						element={<VariantEditorPage />}
+					/>
+					<Route path="/test" element={<TestPage />} />
+					<Route
+						path="/json-validator-test"
+						element={<JSONValidatorTestPage />}
+					/>
+				</Routes>
+			</BrowserRouter>
+		</TooltipProvider>
 	);
 }
 
