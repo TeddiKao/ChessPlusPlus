309cdd0a58f65fadfa0da976c1699c4b3ff3b775
Teddi <teddikao@gmail.com>
2026-03-26 21:00:48 +0800

    Add sidebar icons

--- app/src/features/variants/variantEditor/components/Sidebar.tsx
+++ app/src/features/variants/variantEditor/components/Sidebar.tsx
@@ -1,9 +1,13 @@
 import ChessboardGridIcon from "@/features/variants/icons/ChessboardGridIcon";
+import { ChessKnightIcon } from "lucide-react";
+import { IconArrowsMove } from "@tabler/icons-react";
 
 function Sidebar() {
 	return (
-		<div className="flex flex-col gap-4 right-0 top-0 h-screen bg-sidebar-primary-foreground">
-			<ChessboardGridIcon />
+		<div className="flex flex-col gap-4 items-center absolute right-0 top-0 h-screen bg-sidebar-primary-foreground p-5">
+			<ChessboardGridIcon className="size-5" />
+			<ChessKnightIcon className="size-6" />
+			<IconArrowsMove className="size-6" />
 		</div>
 	);
 }
