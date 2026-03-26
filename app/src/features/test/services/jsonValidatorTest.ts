e77820d74d370fd83cfff8d6974c1ad61459a963
Teddi <teddikao@gmail.com>
2026-03-26 13:07:14 +0800

    Add error handling to JSON validation service function

--- app/src/features/test/services/jsonValidatorTest.ts
+++ app/src/features/test/services/jsonValidatorTest.ts
@@ -1,11 +1,18 @@
 import api from "@/app/api";
+import { AxiosError } from "axios";
 
 async function validateJSON(json: Record<string, unknown>) {
-	const response = await api.post("/json-validator-test", {
-		jsonToValidate: json,
-	});
+	try {
+		const response = await api.post("/json-validator-test", {
+			jsonToValidate: json,
+		});
 
-	return response.data;
+		return response.data;
+	} catch (error) {
+		if (error instanceof AxiosError) {
+			console.error(error.response);
+		}
+	}
 }
 
 export { validateJSON };
