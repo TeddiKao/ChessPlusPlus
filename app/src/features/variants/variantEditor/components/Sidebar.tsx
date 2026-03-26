dec992ae72de55170afa2f43fe843599ec5125e4
Teddi <teddikao@gmail.com>
2026-03-26 21:10:31 +0800

    Add padding to buttons

--- app/src/features/variants/variantEditor/components/Sidebar.tsx
+++ app/src/features/variants/variantEditor/components/Sidebar.tsx
@@ -1,13 +1,20 @@
 import ChessboardGridIcon from "@/features/variants/icons/ChessboardGridIcon";
 import { ChessKnightIcon } from "lucide-react";
 import { IconArrowsMove } from "@tabler/icons-react";
+import { Button } from "@/components/ui/button";
 
 function Sidebar() {
 	return (
-		<div className="flex flex-col gap-4 items-center absolute right-0 top-0 h-screen bg-sidebar-primary-foreground p-5">
-			<ChessboardGridIcon className="size-5" />
-			<ChessKnightIcon className="size-6" />
-			<IconArrowsMove className="size-6" />
+		<div className="flex flex-col gap-4 items-center absolute right-0 top-0 h-screen bg-sidebar-primary-foreground p-3">
+			<Button variant="ghost" className="p-1`">
+				<ChessboardGridIcon className="size-5" />
+			</Button>
+			<Button variant="ghost" className="p-1">
+				<ChessKnightIcon className="size-6" />
+			</Button>
+			<Button variant="ghost" className="p-1">
+				<IconArrowsMove className="size-6" />
+			</Button>
 		</div>
 	);
 }
