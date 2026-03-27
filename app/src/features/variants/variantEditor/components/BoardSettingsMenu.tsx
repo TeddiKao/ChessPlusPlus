6ea295c9f05c5aa154d101b19f261f6b21e516a8
Teddi <teddikao@gmail.com>
2026-03-27 21:21:41 +0800

    Create relevant useEffects to update draft and editor state

--- app/src/features/variants/variantEditor/components/BoardSettingsMenu.tsx
+++ app/src/features/variants/variantEditor/components/BoardSettingsMenu.tsx
@@ -2,8 +2,31 @@ import { Button } from "@/components/ui/button";
 import { Field, FieldLabel, FieldSet } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { PopoverContent } from "@/components/ui/popover";
+import useSetupSettingsEditorStore from "@/features/variants/variantEditor/stores/setupSettingsEditor";
+import { useEffect } from "react";
+import useVariantDraftStore from "@/features/variants/variantEditor/stores/variantDraft";
 
 function BoardSettingsMenu() {
+	const { updateBoardXSize, boardXSize, updateBoardYSize, boardYSize } =
+		useSetupSettingsEditorStore();
+	const { setupRulesDraft } = useVariantDraftStore();
+
+	useEffect(() => {
+		if (!setupRulesDraft) return;
+
+		const initialBoardXSize = setupRulesDraft.boardXSize;
+		if (!initialBoardXSize) return;
+
+		const initialBoardYSize = setupRulesDraft.boardYSize;
+		if (!initialBoardYSize) return;
+
+		updateBoardXSize(initialBoardXSize);
+		updateBoardYSize(initialBoardYSize);
+	}, [setupRulesDraft, updateBoardXSize, updateBoardYSize]);
+
+	if (!boardXSize) return null;
+	if (!boardYSize) return null;
+
 	return (
 		<PopoverContent side="left" sideOffset={8} align="start">
 			<div className="flex flex-col gap-4">
@@ -26,6 +49,7 @@ function BoardSettingsMenu() {
 							id="boardWidthInput"
 							type="number"
 							placeholder="Width"
+							value={boardXSize}
 						/>
 					</Field>
 
@@ -40,6 +64,7 @@ function BoardSettingsMenu() {
 							id="boardHeightInput"
 							type="number"
 							placeholder="Height"
+							value={boardYSize}
 						/>
 					</Field>
 				</FieldSet>
