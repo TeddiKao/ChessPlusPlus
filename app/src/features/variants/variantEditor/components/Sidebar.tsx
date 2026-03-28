9a00b5df998088120dbdd8f312f67afaeebb74de
Teddi <teddikao@gmail.com>
2026-03-28 14:09:44 +0800

    Add movement editor sheet component to sidebar

--- app/src/features/variants/variantEditor/components/Sidebar.tsx
+++ app/src/features/variants/variantEditor/components/Sidebar.tsx
@@ -12,6 +12,7 @@ import BoardSettingsMenu from "./BoardSettingsMenu";
 import useSidebarStore from "../stores/sidebar";
 import useVariantDraftStore from "@/features/variants/variantEditor/stores/variantDraft";
 import useSetupSettingsEditorStore from "@/features/variants/variantEditor/stores/setupSettingsEditor";
+import MovementsEditorSheet from "@/features/variants/variantEditor/components/MovementsEditorSheet";
 
 function Sidebar() {
 	const { currentOpenMenu, updateCurrentOpenMenu, clearCurrentOpenMenu } =
@@ -20,71 +21,81 @@ function Sidebar() {
 	const { commitToDraft } = useSetupSettingsEditorStore();
 
 	return (
-		<div className="flex flex-col gap-4 items-center absolute right-0 top-0 h-screen bg-sidebar-primary-foreground p-3">
-			<Popover
-				open={currentOpenMenu === "boardSettings"}
-				onOpenChange={(open) => {
-					if (open) {
-						updateCurrentOpenMenu("boardSettings");
-					} else {
-						clearCurrentOpenMenu();
+		<>
+			<div className="flex flex-col gap-4 items-center absolute right-0 top-0 h-screen bg-sidebar-primary-foreground p-3">
+				<Popover
+					open={currentOpenMenu === "boardSettings"}
+					onOpenChange={(open) => {
+						if (open) {
+							updateCurrentOpenMenu("boardSettings");
+						} else {
+							clearCurrentOpenMenu();
 
-						commitToDraft();
-						syncSetupRulesDraftToDB();
-					}
-				}}
-			>
+							commitToDraft();
+							syncSetupRulesDraftToDB();
+						}
+					}}
+				>
+					<Tooltip>
+						<TooltipTrigger asChild>
+							<PopoverTrigger asChild>
+								<Button
+									variant="ghost"
+									className="p-1 hover:bg-(--sidebar-primary-hover)"
+									aria-label="Board settings"
+								>
+									<ChessboardGridIcon className="size-5" />
+								</Button>
+							</PopoverTrigger>
+						</TooltipTrigger>
+
+						<TooltipContent side="left" sideOffset={8}>
+							Board settings
+						</TooltipContent>
+					</Tooltip>
+
+					<BoardSettingsMenu />
+				</Popover>
+
 				<Tooltip>
 					<TooltipTrigger asChild>
-						<PopoverTrigger asChild>
-							<Button
-								variant="ghost"
-								className="p-1 hover:bg-(--sidebar-primary-hover)"
-								aria-label="Board settings"
-							>
-								<ChessboardGridIcon className="size-5" />
-							</Button>
-						</PopoverTrigger>
+						<Button
+							variant="ghost"
+							className="p-1 hover:bg-(--sidebar-primary-hover)"
+							aria-label="Pieces"
+						>
+							<ChessKnightIcon
+								strokeWidth={1.5}
+								className="size-6"
+							/>
+						</Button>
 					</TooltipTrigger>
-
 					<TooltipContent side="left" sideOffset={8}>
-						Board settings
+						Pieces
 					</TooltipContent>
 				</Tooltip>
 
-				<BoardSettingsMenu />
-			</Popover>
+				<Tooltip>
+					<TooltipTrigger asChild>
+						<Button
+							variant="ghost"
+							className="p-1 hover:bg-(--sidebar-primary-hover)"
+							aria-label="Movements"
+						>
+							<IconArrowsMove
+								strokeWidth={1.5}
+								className="size-6"
+							/>
+						</Button>
+					</TooltipTrigger>
+					<TooltipContent side="left" sideOffset={8}>
+						Movements
+					</TooltipContent>
+				</Tooltip>
+			</div>
 
-			<Tooltip>
-				<TooltipTrigger asChild>
-					<Button
-						variant="ghost"
-						className="p-1 hover:bg-(--sidebar-primary-hover)"
-						aria-label="Pieces"
-					>
-						<ChessKnightIcon strokeWidth={1.5} className="size-6" />
-					</Button>
-				</TooltipTrigger>
-				<TooltipContent side="left" sideOffset={8}>
-					Pieces
-				</TooltipContent>
-			</Tooltip>
-
-			<Tooltip>
-				<TooltipTrigger asChild>
-					<Button
-						variant="ghost"
-						className="p-1 hover:bg-(--sidebar-primary-hover)"
-						aria-label="Movements"
-					>
-						<IconArrowsMove strokeWidth={1.5} className="size-6" />
-					</Button>
-				</TooltipTrigger>
-				<TooltipContent side="left" sideOffset={8}>
-					Movements
-				</TooltipContent>
-			</Tooltip>
-		</div>
+			<MovementsEditorSheet />
+		</>
 	);
 }
 
