db9c73be5a97bc683c2f3d220d26ca59ddaf0763
Teddi <teddikao@gmail.com>
2026-03-28 14:06:08 +0800

    Add markup for sheet header and footer

--- app/src/features/variants/variantEditor/components/MovementsEditorSheet.tsx
+++ app/src/features/variants/variantEditor/components/MovementsEditorSheet.tsx
@@ -1,3 +1,27 @@
-function MovementsEditorSheet() {}
+import {
+	SheetClose,
+	SheetContent,
+	SheetDescription,
+	SheetFooter,
+	SheetHeader,
+	SheetTitle,
+} from "@/components/ui/sheet";
+
+function MovementsEditorSheet() {
+	return (
+		<SheetContent showCloseButton={false}>
+			<SheetHeader>
+				<SheetTitle>Movements editor</SheetTitle>
+				<SheetDescription>
+					Edit piece movement rules here
+				</SheetDescription>
+			</SheetHeader>
+
+			<SheetFooter>
+				<SheetClose>Close</SheetClose>
+			</SheetFooter>
+		</SheetContent>
+	);
+}
 
 export default MovementsEditorSheet;
