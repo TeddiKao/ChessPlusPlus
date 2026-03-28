742301337710a540fdf284224463213005d1385c
Teddi <teddikao@gmail.com>
2026-03-28 14:35:21 +0800

    Fix p-4 styling to allow hovered elements to appear better

--- app/src/features/variants/variantEditor/components/MovementsEditorSheet.tsx
+++ app/src/features/variants/variantEditor/components/MovementsEditorSheet.tsx
@@ -9,10 +9,14 @@ import {
 } from "@/components/ui/sheet";
 import useSidebarStore from "@/features/variants/variantEditor/stores/sidebar";
 import { Button } from "@/components/ui/button";
+import useVariantDraftStore from "@/features/variants/variantEditor/stores/variantDraft";
 
 function MovementsEditorSheet() {
 	const { currentOpenMenu, updateCurrentOpenMenu, clearCurrentOpenMenu } =
 		useSidebarStore();
+	const { movementRulesDraft } = useVariantDraftStore();
+
+	if (!movementRulesDraft) return null;
 
 	return (
 		<Sheet
@@ -29,14 +33,27 @@ function MovementsEditorSheet() {
 				className="bg-sidebar-primary-foreground"
 				showCloseButton={false}
 			>
-				<SheetHeader>
+				<SheetHeader className="px-4 pt-4">
 					<SheetTitle>Movements editor</SheetTitle>
 					<SheetDescription>
 						Edit piece movement rules here
 					</SheetDescription>
 				</SheetHeader>
 
-				<SheetFooter>
+				<div className="flex flex-col overflow-y-auto px-3">
+					{Object.entries(movementRulesDraft).map(
+						([movementName]) => (
+							<Button
+								className="p-0 px-1 text-left justify-start hover:bg-(--sidebar-primary-hover)"
+								variant="ghost"
+							>
+								{movementName}
+							</Button>
+						),
+					)}
+				</div>
+
+				<SheetFooter className="px-4 pb-4">
 					<SheetClose asChild>
 						<Button variant="outline">Close</Button>
 					</SheetClose>
