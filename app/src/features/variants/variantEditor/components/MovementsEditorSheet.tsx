b644f56425254006267f88f289a4998c2f540db7
Teddi <teddikao@gmail.com>
2026-03-28 18:37:46 +0800

    Extract piece selection screen to a separate function

--- app/src/features/variants/variantEditor/components/MovementsEditorSheet.tsx
+++ app/src/features/variants/variantEditor/components/MovementsEditorSheet.tsx
@@ -11,12 +11,43 @@ import useSidebarStore from "@/features/variants/variantEditor/stores/sidebar";
 import { Button } from "@/components/ui/button";
 import useVariantDraftStore from "@/features/variants/variantEditor/stores/variantDraft";
 
+function PieceSelectionScreen() {
+	const { movementRulesDraft } = useVariantDraftStore();
+	if (!movementRulesDraft) return null;
+
+	return (
+		<>
+			<SheetHeader className="px-4 pt-4">
+				<SheetTitle>Movements editor</SheetTitle>
+				<SheetDescription>
+					Edit piece movement rules here
+				</SheetDescription>
+			</SheetHeader>
+
+			<div className="flex flex-col overflow-y-auto px-3">
+				{Object.entries(movementRulesDraft).map(([movementName]) => (
+					<Button
+						className="p-0 px-1 text-left justify-start hover:bg-(--sidebar-primary-hover)"
+						variant="ghost"
+					>
+						{movementName}
+					</Button>
+				))}
+			</div>
+
+			<SheetFooter className="px-4 pb-4">
+				<Button>Create movement</Button>
+				<SheetClose asChild>
+					<Button variant="outline">Close</Button>
+				</SheetClose>
+			</SheetFooter>
+		</>
+	);
+}
+
 function MovementsEditorSheet() {
 	const { currentOpenMenu, updateCurrentOpenMenu, clearCurrentOpenMenu } =
 		useSidebarStore();
-	const { movementRulesDraft } = useVariantDraftStore();
-
-	if (!movementRulesDraft) return null;
 
 	return (
 		<Sheet
@@ -33,32 +64,7 @@ function MovementsEditorSheet() {
 				className="bg-sidebar-primary-foreground"
 				showCloseButton={false}
 			>
-				<SheetHeader className="px-4 pt-4">
-					<SheetTitle>Movements editor</SheetTitle>
-					<SheetDescription>
-						Edit piece movement rules here
-					</SheetDescription>
-				</SheetHeader>
-
-				<div className="flex flex-col overflow-y-auto px-3">
-					{Object.entries(movementRulesDraft).map(
-						([movementName]) => (
-							<Button
-								className="p-0 px-1 text-left justify-start hover:bg-(--sidebar-primary-hover)"
-								variant="ghost"
-							>
-								{movementName}
-							</Button>
-						),
-					)}
-				</div>
-
-				<SheetFooter className="px-4 pb-4">
-					<Button>Create movement</Button>
-					<SheetClose asChild>
-						<Button variant="outline">Close</Button>
-					</SheetClose>
-				</SheetFooter>
+				<PieceSelectionScreen />
 			</SheetContent>
 		</Sheet>
 	);
