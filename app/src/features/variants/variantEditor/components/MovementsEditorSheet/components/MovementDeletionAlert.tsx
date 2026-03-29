8674546c0431b8a0c61215a9d82ee50d8fc6d83f
Teddi <teddikao@gmail.com>
2026-03-29 11:10:01 +0800

    Attach open and onOpenChange handlers to delete movement alert

--- app/src/features/variants/variantEditor/components/MovementsEditorSheet/components/MovementDeletionAlert.tsx
+++ app/src/features/variants/variantEditor/components/MovementsEditorSheet/components/MovementDeletionAlert.tsx
@@ -1,9 +1,18 @@
 import { AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle } from "@/components/ui/alert-dialog";
 import { AlertDialog } from "@/components/ui/alert-dialog";
+import useDeleteMovementAlertStore from "@/features/variants/variantEditor/stores/deleteMovementAlert";
 
 function MovementDeletionAlert() {
+    const { isDeleteMovementAlertOpen, openDeleteMovementAlert, closeDeleteMovementAlert } = useDeleteMovementAlertStore();
+
     return (
-        <AlertDialog open={true}>
+        <AlertDialog open={isDeleteMovementAlertOpen} onOpenChange={(open) => {
+            if (open) {
+                openDeleteMovementAlert();
+            } else {
+                closeDeleteMovementAlert();
+            }
+        }}>
             <AlertDialogContent>
                 <AlertDialogHeader>
                     <AlertDialogTitle>Delete movement?</AlertDialogTitle>
