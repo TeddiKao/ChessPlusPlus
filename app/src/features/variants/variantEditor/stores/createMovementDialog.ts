6e74a145f56a71bc2a2d8d10603ae4a91daf2e22
Teddi <teddikao@gmail.com>
2026-03-29 10:38:59 +0800

    Add additional state to store to track inputs

--- app/src/features/variants/variantEditor/stores/createMovementDialog.ts
+++ app/src/features/variants/variantEditor/stores/createMovementDialog.ts
@@ -4,12 +4,20 @@ type CreateMovementDialogStore = {
     isCreateMovementDialogOpen: boolean;
     openCreateMovementDialog: () => void;
     closeCreateMovementDialog: () => void;
+
+    movementName: string;
+    updateMovementName: (movementName: string) => void;
+    clearMovementName: () => void;
 }
 
 const useCreateMovementDialogStore = create<CreateMovementDialogStore>((set) => ({
     isCreateMovementDialogOpen: false,
     openCreateMovementDialog: () => set({ isCreateMovementDialogOpen: true }),
     closeCreateMovementDialog: () => set({ isCreateMovementDialogOpen: false }),
+    
+    movementName: "",
+    updateMovementName: (movementName: string) => set({ movementName }),
+    clearMovementName: () => set({ movementName: "" }),
 }));
 
 export default useCreateMovementDialogStore;
