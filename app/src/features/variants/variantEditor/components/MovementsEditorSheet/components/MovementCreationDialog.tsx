6a37ba594c084651b51af95cea34ba03001b6851
Teddi <teddikao@gmail.com>
2026-03-29 10:48:01 +0800

    Add FieldError component

--- app/src/features/variants/variantEditor/components/MovementsEditorSheet/components/MovementCreationDialog.tsx
+++ app/src/features/variants/variantEditor/components/MovementsEditorSheet/components/MovementCreationDialog.tsx
@@ -1,36 +1,62 @@
 import { Button } from "@/components/ui/button";
-import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog";
-import { Field, FieldLabel } from "@/components/ui/field";
+import {
+	Dialog,
+	DialogContent,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+} from "@/components/ui/dialog";
+import { Field, FieldError, FieldLabel } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import useCreateMovementDialogStore from "@/features/variants/variantEditor/stores/createMovementDialog";
 
 function MovementCreationDialog() {
-    const { isCreateMovementDialogOpen, openCreateMovementDialog, closeCreateMovementDialog } = useCreateMovementDialogStore();
+	const {
+		isCreateMovementDialogOpen,
+		openCreateMovementDialog,
+		closeCreateMovementDialog,
+		movementNameErrors,
+	} = useCreateMovementDialogStore();
 
-    return (
-        <Dialog open={isCreateMovementDialogOpen} onOpenChange={(open) => {
-            if (open) {
-                openCreateMovementDialog();
-            } else {
-                closeCreateMovementDialog();
-            }
-        }}>
-            <DialogContent>
-                <DialogHeader>
-                    <DialogTitle>Create Movement</DialogTitle>
-                </DialogHeader>
+	return (
+		<Dialog
+			open={isCreateMovementDialogOpen}
+			onOpenChange={(open) => {
+				if (open) {
+					openCreateMovementDialog();
+				} else {
+					closeCreateMovementDialog();
+				}
+			}}
+		>
+			<DialogContent>
+				<DialogHeader>
+					<DialogTitle>Create Movement</DialogTitle>
+				</DialogHeader>
 
-                <Field className="gap-2">
-                    <FieldLabel htmlFor="movementNameInput">Movement Name</FieldLabel>
-                    <Input id="movementNameInput" type="text" placeholder="Enter movement name" />
-                </Field>
+				<Field data-invalid={movementNameErrors.length > 0} className="gap-2">
+					<FieldLabel htmlFor="movementNameInput">
+						Movement Name
+					</FieldLabel>
+					<Input
+						id="movementNameInput"
+						type="text"
+						placeholder="Enter movement name"
+						aria-invalid={movementNameErrors.length > 0}
+					/>
+					<FieldError
+						errors={movementNameErrors.map((errorMessage) => ({
+							message: errorMessage,
+						}))}
+					/>
+				</Field>
 
-                <DialogFooter>
-                    <Button className="px-4">Create</Button>
-                </DialogFooter>
-            </DialogContent>
-        </Dialog>
-    );
+				<DialogFooter>
+					<Button className="px-4">Create</Button>
+				</DialogFooter>
+			</DialogContent>
+		</Dialog>
+	);
 }
 
 export default MovementCreationDialog;
