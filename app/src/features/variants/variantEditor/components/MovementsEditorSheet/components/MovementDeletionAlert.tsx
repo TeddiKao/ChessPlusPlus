5096006ca6243ec18db9eeab52423120e821c4a5
Teddi <teddikao@gmail.com>
2026-03-29 11:03:42 +0800

    Add markup for movement deletion alert dialog

--- app/src/features/variants/variantEditor/components/MovementsEditorSheet/components/MovementDeletionAlert.tsx
+++ app/src/features/variants/variantEditor/components/MovementsEditorSheet/components/MovementDeletionAlert.tsx
@@ -1,8 +1,23 @@
+import { AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle } from "@/components/ui/alert-dialog";
+import { AlertDialog } from "@/components/ui/alert-dialog";
+
 function MovementDeletionAlert() {
     return (
-        <div>
-            <p>Are you sure you want to delete this movement?</p>
-        </div>
+        <AlertDialog open={true}>
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                    <AlertDialogTitle>Delete movement?</AlertDialogTitle>
+                    <AlertDialogDescription>
+                        Are you sure you want to delete this movement? This action cannot be undone.
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+                
+                <AlertDialogFooter>
+                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+                    <AlertDialogAction>Delete</AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
     );
 }
 
