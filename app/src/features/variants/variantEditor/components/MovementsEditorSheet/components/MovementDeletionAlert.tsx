06ebe2eec82e2ee9bf16903f50e2191280ab863c
Teddi <teddikao@gmail.com>
2026-03-29 11:16:51 +0800

    Create basic deletion logic

--- app/src/features/variants/variantEditor/components/MovementsEditorSheet/components/MovementDeletionAlert.tsx
+++ app/src/features/variants/variantEditor/components/MovementsEditorSheet/components/MovementDeletionAlert.tsx
@@ -1,33 +1,75 @@
-import { AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle } from "@/components/ui/alert-dialog";
+import {
+	AlertDialogAction,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
 import { AlertDialog } from "@/components/ui/alert-dialog";
 import useDeleteMovementAlertStore from "@/features/variants/variantEditor/stores/deleteMovementAlert";
+import useVariantDraftStore from "@/features/variants/variantEditor/stores/variantDraft";
 
 function MovementDeletionAlert() {
-    const { isDeleteMovementAlertOpen, openDeleteMovementAlert, closeDeleteMovementAlert } = useDeleteMovementAlertStore();
+	const {
+		isDeleteMovementAlertOpen,
+		openDeleteMovementAlert,
+		closeDeleteMovementAlert,
+		movementToDelete,
+        clearMovementToDelete
+	} = useDeleteMovementAlertStore();
+	const { movementRulesDraft, updateMovementRulesDraft } =
+		useVariantDraftStore();
 
-    return (
-        <AlertDialog open={isDeleteMovementAlertOpen} onOpenChange={(open) => {
-            if (open) {
-                openDeleteMovementAlert();
-            } else {
-                closeDeleteMovementAlert();
-            }
-        }}>
-            <AlertDialogContent>
-                <AlertDialogHeader>
-                    <AlertDialogTitle>Delete movement?</AlertDialogTitle>
-                    <AlertDialogDescription>
-                        Are you sure you want to delete this movement? This action cannot be undone.
-                    </AlertDialogDescription>
-                </AlertDialogHeader>
-                
-                <AlertDialogFooter>
-                    <AlertDialogCancel className="px-4">Cancel</AlertDialogCancel>
-                    <AlertDialogAction className="px-4">Delete</AlertDialogAction>
-                </AlertDialogFooter>
-            </AlertDialogContent>
-        </AlertDialog>
-    );
+	if (!movementRulesDraft) return null;
+
+	function handleMovementDelete() {
+		if (!movementRulesDraft) return;
+		if (!movementToDelete) return;
+
+		const newMovementRulesDraft = structuredClone(movementRulesDraft);
+		delete newMovementRulesDraft[movementToDelete];
+
+		updateMovementRulesDraft(newMovementRulesDraft);
+		closeDeleteMovementAlert();
+	}
+
+	return (
+		<AlertDialog
+			open={isDeleteMovementAlertOpen}
+			onOpenChange={(open) => {
+				if (open) {
+					openDeleteMovementAlert();
+				} else {
+					closeDeleteMovementAlert();
+					clearMovementToDelete();
+				}
+			}}
+		>
+			<AlertDialogContent>
+				<AlertDialogHeader>
+					<AlertDialogTitle>Delete movement?</AlertDialogTitle>
+					<AlertDialogDescription>
+						Are you sure you want to delete this movement? This
+						action cannot be undone.
+					</AlertDialogDescription>
+				</AlertDialogHeader>
+
+				<AlertDialogFooter>
+					<AlertDialogCancel className="px-4">
+						Cancel
+					</AlertDialogCancel>
+					<AlertDialogAction
+						className="px-4"
+						onClick={handleMovementDelete}
+					>
+						Delete
+					</AlertDialogAction>
+				</AlertDialogFooter>
+			</AlertDialogContent>
+		</AlertDialog>
+	);
 }
 
 export default MovementDeletionAlert;
