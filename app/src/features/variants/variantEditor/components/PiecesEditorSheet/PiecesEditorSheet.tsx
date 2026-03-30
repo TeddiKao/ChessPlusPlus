b582186ae0068946bfb92dcb18aa3c3bbeb92fa7
Teddi <teddikao@gmail.com>
2026-03-30 16:28:43 +0800

    Hide close button in sheet

--- app/src/features/variants/variantEditor/components/PiecesEditorSheet/PiecesEditorSheet.tsx
+++ app/src/features/variants/variantEditor/components/PiecesEditorSheet/PiecesEditorSheet.tsx
@@ -24,7 +24,7 @@ function PiecesEditorSheet() {
 				}
 			}}
 		>
-			<SheetContent>
+			<SheetContent showCloseButton={false}>
 				<SheetHeader>
 					<SheetTitle>Pieces Editor</SheetTitle>
 					<SheetDescription>
