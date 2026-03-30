e3e20e811906e549bcf978356c913e9edcc60c9c
Teddi <teddikao@gmail.com>
2026-03-30 17:04:34 +0800

    Add piece editor screen to editor sheet

--- app/src/features/variants/variantEditor/components/PiecesEditorSheet/PiecesEditorSheet.tsx
+++ app/src/features/variants/variantEditor/components/PiecesEditorSheet/PiecesEditorSheet.tsx
@@ -2,10 +2,13 @@ import { Sheet, SheetContent } from "@/components/ui/sheet";
 import useSidebarStore from "@/features/variants/variantEditor/stores/sidebar";
 import useVariantDraftStore from "@/features/variants/variantEditor/stores/variantDraft";
 import { PieceSelectionScreen } from "@/features/variants/variantEditor/components/PiecesEditorSheet/components/PieceSelectionScreen";
+import usePiecesEditorSheetStore from "@/features/variants/variantEditor/stores/piecesEditorSheet";
+import PieceEditorScreen from "@/features/variants/variantEditor/components/PiecesEditorSheet/components/PieceEditorScreen";
 
 function PiecesEditorSheet() {
 	const { currentOpenMenu, updateCurrentOpenMenu, clearCurrentOpenMenu } =
 		useSidebarStore();
+	const { currentMode } = usePiecesEditorSheetStore();
 
 	const { pieceRulesetDraft } = useVariantDraftStore();
 	if (!pieceRulesetDraft) return null;
@@ -25,7 +28,11 @@ function PiecesEditorSheet() {
 				className="bg-sidebar-primary-foreground"
 				showCloseButton={false}
 			>
-				<PieceSelectionScreen />
+				{currentMode === "pieceSelection" ? (
+					<PieceSelectionScreen />
+				) : (
+					<PieceEditorScreen />
+				)}
 			</SheetContent>
 		</Sheet>
 	);
