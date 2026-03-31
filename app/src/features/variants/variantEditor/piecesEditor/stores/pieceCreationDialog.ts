49790f7b5cdbc833c0f9df519109f57e25ca4f59
Teddi <teddikao@gmail.com>
2026-03-31 20:49:46 +0800

    Remove piece name and error clearing functions as they are handled when closing dialogv

--- app/src/features/variants/variantEditor/piecesEditor/stores/pieceCreationDialog.ts
+++ app/src/features/variants/variantEditor/piecesEditor/stores/pieceCreationDialog.ts
@@ -7,25 +7,21 @@ type PieceCreationDialogStore = {
 
 	pieceName: string;
 	updatePieceName: (name: string) => void;
-	clearPieceName: () => void;
 
 	pieceNameErrors: string[];
 	updatePieceNameErrors: (errors: string[]) => void;
-	clearPieceNameErrors: () => void;
 }
 
 const usePieceCreationDialogStore = create<PieceCreationDialogStore>((set) => ({
 	isPieceCreationDialogOpen: false,
 	openPieceCreationDialog: () => set({ isPieceCreationDialogOpen: true }),
-	closePieceCreationDialog: () => set({ isPieceCreationDialogOpen: false }),
-
+	closePieceCreationDialog: () => set({ isPieceCreationDialogOpen: false, pieceName: "", pieceNameErrors: [] }),
+	
 	pieceName: "",
 	updatePieceName: (name) => set({ pieceName: name }),
-	clearPieceName: () => set({ pieceName: "" }),
-
+	
 	pieceNameErrors: [],
 	updatePieceNameErrors: (errors) => set({ pieceNameErrors: errors }),
-	clearPieceNameErrors: () => set({ pieceNameErrors: [] }),
 }));
 
 export default usePieceCreationDialogStore;
