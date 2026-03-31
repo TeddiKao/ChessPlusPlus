d8057bdcb2283a0e552cabb373cf887709323504
Teddi <teddikao@gmail.com>
2026-04-01 07:41:24 +0800

    Click file upload input ref when button is clicked

--- app/src/features/variants/variantEditor/piecesEditor/components/PiecesEditorSheet/components/PieceEditorScreen/components/AppearanceTab.tsx
+++ app/src/features/variants/variantEditor/piecesEditor/components/PiecesEditorSheet/components/PieceEditorScreen/components/AppearanceTab.tsx
@@ -1,5 +1,5 @@
 import usePiecesEditorStore from "@/features/variants/variantEditor/piecesEditor/stores/piecesEditor";
-import type { ChangeEvent } from "react";
+import { useRef, type ChangeEvent } from "react";
 import { Field, FieldLabel } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
@@ -8,12 +8,19 @@ import { TabsContent } from "@/components/ui/tabs";
 
 export function AppearanceTab() {
 	const { pieceName, updatePieceName } = usePiecesEditorStore();
+	const fileUploadInputRef = useRef<HTMLInputElement>(null);
+
+	if (!pieceName) return null;
 
 	function handlePieceNameInputChange(e: ChangeEvent<HTMLInputElement>) {
 		updatePieceName(e.target.value);
 	}
 
-	if (!pieceName) return null;
+	function handleUploadImageButtonClick() {
+		if (!fileUploadInputRef.current) return;
+
+		fileUploadInputRef.current.click();
+	}
 
 	return (
 		<TabsContent className="flex flex-col gap-4" value="appearance">
@@ -31,10 +38,12 @@ export function AppearanceTab() {
 
 			<div className="grid grid-cols-2 gap-4">
 				<p>Piece image</p>
-				<Button data-icon="inline-start" variant="outline">
+				<Button data-icon="inline-start" variant="outline" onClick={handleUploadImageButtonClick}>
 					<IconUpload className="size-5" />
 					<span>Upload image</span>
 				</Button>
+
+				<Input type="file" ref={fileUploadInputRef} className="hidden" />
 			</div>
 		</TabsContent>
 	);
