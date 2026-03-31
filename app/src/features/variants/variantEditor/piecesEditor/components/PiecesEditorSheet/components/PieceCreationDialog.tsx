f530f3f98f99652688e4f330ce2a2fc95ad8ee98
Teddi <teddikao@gmail.com>
2026-03-31 20:55:58 +0800

    Add logic for piece creation

--- app/src/features/variants/variantEditor/piecesEditor/components/PiecesEditorSheet/components/PieceCreationDialog.tsx
+++ app/src/features/variants/variantEditor/piecesEditor/components/PiecesEditorSheet/components/PieceCreationDialog.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { Field, FieldError, FieldLabel } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
+import useVariantDraftStore from "@/features/variants/variantEditor/common/stores/variantDraft";
 import usePieceCreationDialogStore from "@/features/variants/variantEditor/piecesEditor/stores/pieceCreationDialog";
 import type { ChangeEvent } from "react";
 
@@ -13,11 +14,39 @@ function PieceCreationDialog() {
 		closePieceCreationDialog,
 		pieceName,
 		updatePieceName,
+		updatePieceNameErrors,
 		pieceNameErrors,
 	} = usePieceCreationDialogStore();
 
+	const { pieceRulesetDraft, updatePieceRulesetDraft } = useVariantDraftStore();
+
+	if (!pieceRulesetDraft) return null;
+
 	function handlePieceNameInputChange(e: ChangeEvent<HTMLInputElement>) {
 		updatePieceName(e.target.value);
+	}
+
+	function handlePieceCreation() {
+		if (!pieceRulesetDraft) return;
+		
+		if (pieceName.trim() === "") {
+			updatePieceNameErrors(["Piece name is required"]);
+			return;
+		}
+
+		if (Object.keys(pieceRulesetDraft).includes(pieceName.trim())) {
+			updatePieceNameErrors(["Piece name already exists"]);
+			return;
+		}
+
+		const updatedPieceRulesetDraft = structuredClone(pieceRulesetDraft);
+		updatedPieceRulesetDraft[pieceName.trim()] = {
+			moveset: [],
+		};
+
+		updatePieceRulesetDraft(updatedPieceRulesetDraft);
+
+		closePieceCreationDialog();
 	}
 	
 	return (
@@ -51,7 +80,7 @@ function PieceCreationDialog() {
 				</Field>
 
 				<DialogFooter>
-					<Button className="px-4">Create</Button>
+					<Button onClick={handlePieceCreation} className="px-4">Create</Button>
 				</DialogFooter>
 			</DialogContent>
 		</Dialog>
