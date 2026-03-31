224b85eb908a92063b1ae4c7f913c5bb44766cda
Teddi <teddikao@gmail.com>
2026-03-31 21:05:54 +0800

    Add additional state to piece deletion alert to track piece to delete

--- app/src/features/variants/variantEditor/piecesEditor/stores/pieceDeletionAlert.ts
+++ app/src/features/variants/variantEditor/piecesEditor/stores/pieceDeletionAlert.ts
@@ -4,12 +4,18 @@ type PieceDeletionAlertStore = {
 	isPieceDeletionAlertOpen: boolean;
 	openPieceDeletionAlert: () => void;
 	closePieceDeletionAlert: () => void;
+
+	pieceToDelete: string | null;
+	updatePieceToDelete: (piece: string) => void;
 }
 
 const usePieceDeletionAlertStore = create<PieceDeletionAlertStore>((set) => ({
 	isPieceDeletionAlertOpen: false,
 	openPieceDeletionAlert: () => set({ isPieceDeletionAlertOpen: true }),
-	closePieceDeletionAlert: () => set({ isPieceDeletionAlertOpen: false }),
+	closePieceDeletionAlert: () => set({ isPieceDeletionAlertOpen: false, pieceToDelete: null }),
+
+	pieceToDelete: null,
+	updatePieceToDelete: (piece) => set({ pieceToDelete: piece }),
 }));
 
 export default usePieceDeletionAlertStore;
