19b928bc08affc05d7a9ccafd2b1eef720622979
Teddi <teddikao@gmail.com>
2026-03-31 20:51:43 +0800

    Add functionality to open dialog when button is clicked

--- app/src/features/variants/variantEditor/piecesEditor/components/PiecesEditorSheet/components/PieceSelectionScreen.tsx
+++ app/src/features/variants/variantEditor/piecesEditor/components/PiecesEditorSheet/components/PieceSelectionScreen.tsx
@@ -1,5 +1,6 @@
 import useVariantDraftStore from "@/features/variants/variantEditor/common/stores/variantDraft";
 import {
+	SheetClose,
 	SheetDescription,
 	SheetFooter,
 	SheetHeader,
@@ -8,11 +9,14 @@ import {
 import { Button } from "@/components/ui/button";
 import usePiecesEditorSheetStore from "@/features/variants/variantEditor/piecesEditor/stores/piecesEditorSheet";
 import usePiecesEditorStore from "@/features/variants/variantEditor/piecesEditor/stores/piecesEditor";
+import PieceCreationDialog from "@/features/variants/variantEditor/piecesEditor/components/PiecesEditorSheet/components/PieceCreationDialog";
+import usePieceCreationDialogStore from "@/features/variants/variantEditor/piecesEditor/stores/pieceCreationDialog";
 
 export function PieceSelectionScreen() {
 	const { pieceRulesetDraft } = useVariantDraftStore();
 	const { updateCurrentMode } = usePiecesEditorSheetStore();
 	const { updateActivePiece } = usePiecesEditorStore();
+	const { openPieceCreationDialog } = usePieceCreationDialogStore();
 
 	if (!pieceRulesetDraft) return null;
 
@@ -23,30 +27,36 @@ export function PieceSelectionScreen() {
 
 	return (
 		<>
-			<SheetHeader>
-				<SheetTitle>Pieces Editor</SheetTitle>
-				<SheetDescription>
-					Edit the pieces in this variant.
-				</SheetDescription>
-			</SheetHeader>
+			<>
+				<SheetHeader>
+					<SheetTitle>Pieces Editor</SheetTitle>
+					<SheetDescription>
+						Edit the pieces in this variant.
+					</SheetDescription>
+				</SheetHeader>
 
-			<div className="flex flex-col px-3">
-				{Object.keys(pieceRulesetDraft).map((piece) => (
-					<Button
-						className="p-0 px-1 justify-start hover:bg-(--sidebar-primary-hover)"
-						variant="ghost"
-						key={piece}
-						onClick={() => handlePieceClick(piece)}
-					>
-						{piece}
-					</Button>
-				))}
-			</div>
+				<div className="flex flex-col px-3">
+					{Object.keys(pieceRulesetDraft).map((piece) => (
+						<Button
+							className="p-0 px-1 justify-start hover:bg-(--sidebar-primary-hover)"
+							variant="ghost"
+							key={piece}
+							onClick={() => handlePieceClick(piece)}
+						>
+							{piece}
+						</Button>
+					))}
+				</div>
 
-			<SheetFooter>
-				<Button>Create piece</Button>
-				<Button variant="outline">Close</Button>
-			</SheetFooter>
+				<SheetFooter>
+					<Button onClick={openPieceCreationDialog}>Create piece</Button>
+					<SheetClose asChild>
+						<Button variant="outline">Close</Button>
+					</SheetClose>
+				</SheetFooter>
+			</>
+
+			<PieceCreationDialog />
 		</>
 	);
 }
