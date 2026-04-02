723486d31de5ad95f07c9b36541b8e948837f469
Teddi <teddikao@gmail.com>
2026-04-02 17:22:08 +0800

    Create default images when creating a default variant

--- app/src/features/variants/variantCreation/components/CreateVariantDialog.tsx
+++ app/src/features/variants/variantCreation/components/CreateVariantDialog.tsx
@@ -13,6 +13,8 @@ import { Label } from "@/components/ui/label";
 import useVariantsStore from "@/features/variants/common/stores/variantsStore";
 import type { VariantInfo } from "@/features/variants/common/types/variants";
 import { defaultVariantRules } from "@/features/variants/variantCreation/constants/newVariantDefaults";
+import { defaultPieceImages } from "@/features/variants/variantCreation/constants/defaultPieceImages";
+import usePieceImagesStore from "@/features/variants/common/stores/pieceImages";
 
 function CreateVariantDialog() {
 	const {
@@ -25,6 +27,11 @@ function CreateVariantDialog() {
 	} = useCreateVariantDialogStore();
 
 	const { createVariant, hasHydrated } = useVariantsStore();
+	const {
+		defaultImagesCreated,
+		markAsDefaultImagesCreated,
+		updateImages,
+	} = usePieceImagesStore();
 
 	if (!hasHydrated) return null;
 
@@ -41,6 +48,11 @@ function CreateVariantDialog() {
 		};
 
 		createVariant(defaultVariant);
+
+		if (defaultImagesCreated) return;
+
+		updateImages(defaultPieceImages);
+		markAsDefaultImagesCreated();
 
 		clearVariantName();
 		closeDialog();
