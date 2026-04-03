289eb8f17e34321c52fb0a6cc8e23f59d3d3a2dd
Teddi <teddikao@gmail.com>
2026-04-03 14:12:56 +0800

    Add state to track selected movements

--- app/src/features/variants/variantEditor/piecesEditor/stores/movementSelectionDialog.ts
+++ app/src/features/variants/variantEditor/piecesEditor/stores/movementSelectionDialog.ts
@@ -4,12 +4,22 @@ type MovementSelectionDialogStore = {
 	isMovementSelectionDialogOpen: boolean;
 	openMovementSelectionDialog: () => void;
 	closeMovementSelectionDialog: () => void;
+
+	selectedMovements: string[];
+	selectMovement: (movement: string) => void;
+	deselectMovement: (movement: string) => void;
+	clearSelectedMovements: () => void;
 }
 
 const useMovementSelectionDialogStore = create<MovementSelectionDialogStore>((set) => ({
 	isMovementSelectionDialogOpen: false,
 	openMovementSelectionDialog: () => set({ isMovementSelectionDialogOpen: true }),
 	closeMovementSelectionDialog: () => set({ isMovementSelectionDialogOpen: false }),
+
+	selectedMovements: [],
+	selectMovement: (movement: string) => set((state) => ({ selectedMovements: [...state.selectedMovements, movement] })),
+	deselectMovement: (movement: string) => set((state) => ({ selectedMovements: state.selectedMovements.filter((m) => m !== movement) })),
+	clearSelectedMovements: () => set({ selectedMovements: [] }),
 }))
 
 export default useMovementSelectionDialogStore;
