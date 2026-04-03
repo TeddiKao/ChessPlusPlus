66ba866731e6b3494734d13577f0538206068b7b
Teddi <teddikao@gmail.com>
2026-04-03 14:23:26 +0800

    Add open and onOpenChange handlers to dialog

--- app/src/features/variants/variantEditor/piecesEditor/components/PiecesEditorSheet/components/PieceEditorScreen/components/MovementsTab/MovementSelectionDialog.tsx
+++ app/src/features/variants/variantEditor/piecesEditor/components/PiecesEditorSheet/components/PieceEditorScreen/components/MovementsTab/MovementSelectionDialog.tsx
@@ -1,19 +1,50 @@
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/components/ui/dialog";
-import { InputGroup, InputGroupAddon, InputGroupInput } from "@/components/ui/input-group";
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogHeader,
+	DialogTitle,
+} from "@/components/ui/dialog";
+import {
+	InputGroup,
+	InputGroupAddon,
+	InputGroupInput,
+} from "@/components/ui/input-group";
+import useMovementSelectionDialogStore from "@/features/variants/variantEditor/piecesEditor/stores/movementSelectionDialog";
 import { IconSearch } from "@tabler/icons-react";
 
 function MovementSelectionDialog() {
+	const {
+		isMovementSelectionDialogOpen,
+		openMovementSelectionDialog,
+		closeMovementSelectionDialog,
+	} = useMovementSelectionDialogStore();
+
 	return (
-		<Dialog>
+		<Dialog
+			open={isMovementSelectionDialogOpen}
+			onOpenChange={(isOpen) => {
+				if (isOpen) {
+					openMovementSelectionDialog();
+				} else {
+					closeMovementSelectionDialog();
+				}
+			}}
+		>
 			<DialogContent>
 				<DialogHeader>
 					<DialogTitle>Select movements</DialogTitle>
-					<DialogDescription>Select the movements you want to add to the piece.</DialogDescription>
+					<DialogDescription>
+						Select the movements you want to add to the piece.
+					</DialogDescription>
 				</DialogHeader>
 
 				<div className="flex flex-col">
 					<InputGroup>
-						<InputGroupInput type="text" placeholder="Search movements" />
+						<InputGroupInput
+							type="text"
+							placeholder="Search movements"
+						/>
 						<InputGroupAddon align="inline-start">
 							<IconSearch />
 						</InputGroupAddon>
@@ -21,7 +52,7 @@ function MovementSelectionDialog() {
 				</div>
 			</DialogContent>
 		</Dialog>
-	)
+	);
 }
 
 export default MovementSelectionDialog;
