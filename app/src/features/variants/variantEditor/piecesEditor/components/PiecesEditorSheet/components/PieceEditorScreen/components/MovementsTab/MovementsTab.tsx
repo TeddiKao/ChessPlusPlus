07da5587193fec376300be9f6dc5e3aa1f6fd79e
Teddi <teddikao@gmail.com>
2026-04-03 12:13:55 +0800

    Remove movement add dialog from movements tab

--- app/src/features/variants/variantEditor/piecesEditor/components/PiecesEditorSheet/components/PieceEditorScreen/components/MovementsTab/MovementsTab.tsx
+++ app/src/features/variants/variantEditor/piecesEditor/components/PiecesEditorSheet/components/PieceEditorScreen/components/MovementsTab/MovementsTab.tsx
@@ -18,7 +18,6 @@ import {
 	DropdownMenuContent,
 	DropdownMenuItem,
 } from "@/components/ui/dropdown-menu";
-import MovementAddDialog from "@/features/variants/variantEditor/piecesEditor/components/PiecesEditorSheet/components/PieceEditorScreen/components/MovementsTab/MovementAddDialog";
 
 export function MovementsTab() {
 	const {
@@ -29,68 +28,64 @@ export function MovementsTab() {
 	} = usePiecesEditorStore();
 
 	return (
-		<>
-			<TabsContent value="movements">
-				<Collapsible
-					className="flex flex-col gap-1"
-					open={isMovementsExpanded}
-					onOpenChange={(open) => {
-						if (open) {
-							expandMovements();
-						} else {
-							collapseMovements();
-						}
-					}}
-				>
-					<div className="flex flex-row items-center justify-between">
-						<p className="font-semibold">Movements</p>
+		<TabsContent value="movements">
+			<Collapsible
+				className="flex flex-col gap-1"
+				open={isMovementsExpanded}
+				onOpenChange={(open) => {
+					if (open) {
+						expandMovements();
+					} else {
+						collapseMovements();
+					}
+				}}
+			>
+				<div className="flex flex-row items-center justify-between">
+					<p className="font-semibold">Movements</p>
 
-						<CollapsibleTrigger asChild>
-							<Button
-								variant="ghost"
-								className="p-0 px-1 hover:bg-(--sidebar-primary-hover) hover:aria-expanded:bg-(--sidebar-primary-hover)"
-							>
-								{isMovementsExpanded ? (
-									<IconChevronUp className="size-4" />
-								) : (
-									<IconChevronDown className="size-4" />
-								)}
-							</Button>
-						</CollapsibleTrigger>
-					</div>
+					<CollapsibleTrigger asChild>
+						<Button
+							variant="ghost"
+							className="p-0 px-1 hover:bg-(--sidebar-primary-hover) hover:aria-expanded:bg-(--sidebar-primary-hover)"
+						>
+							{isMovementsExpanded ? (
+								<IconChevronUp className="size-4" />
+							) : (
+								<IconChevronDown className="size-4" />
+							)}
+						</Button>
+					</CollapsibleTrigger>
+				</div>
 
-					<CollapsibleContent className="flex flex-col overflow-y-auto">
-						{activePieceMovements.map((movement) => (
-							<div
-								className="flex flex-row items-center justify-between"
-								key={movement.moveName}
-							>
-								<p>{movement.moveName}</p>
+				<CollapsibleContent className="flex flex-col overflow-y-auto">
+					{activePieceMovements.map((movement) => (
+						<div
+							className="flex flex-row items-center justify-between"
+							key={movement.moveName}
+						>
+							<p>{movement.moveName}</p>
 
-								<DropdownMenu>
-									<DropdownMenuTrigger asChild>
-										<Button
-											variant="ghost"
-											className="p-0 hover:bg-(--sidebar-primary-hover)"
-										>
-											<IconDotsVertical className="size-5" />
-										</Button>
-									</DropdownMenuTrigger>
+							<DropdownMenu>
+								<DropdownMenuTrigger asChild>
+									<Button
+										variant="ghost"
+										className="p-0 hover:bg-(--sidebar-primary-hover)"
+									>
+										<IconDotsVertical className="size-5" />
+									</Button>
+								</DropdownMenuTrigger>
 
-									<DropdownMenuContent side="left">
-										<DropdownMenuItem variant="destructive">
-											<IconX className="size-4" />
-											Remove
-										</DropdownMenuItem>
-									</DropdownMenuContent>
-								</DropdownMenu>
-							</div>
-						))}
-					</CollapsibleContent>
-				</Collapsible>
-			</TabsContent>
-			
-			<MovementAddDialog />
-		</>
+								<DropdownMenuContent side="left">
+									<DropdownMenuItem variant="destructive">
+										<IconX className="size-4" />
+										Remove
+									</DropdownMenuItem>
+								</DropdownMenuContent>
+							</DropdownMenu>
+						</div>
+					))}
+				</CollapsibleContent>
+			</Collapsible>
+		</TabsContent>
 	);
 }
