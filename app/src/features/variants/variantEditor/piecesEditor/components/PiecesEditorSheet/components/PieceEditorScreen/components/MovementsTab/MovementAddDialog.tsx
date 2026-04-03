5fcab8cc64264a68f2b8b92b3f5fd24ae988bb70
Teddi <teddikao@gmail.com>
2026-04-03 12:12:28 +0800

    Add logic to display movement names added using badges in shadcn dialog

--- app/src/features/variants/variantEditor/piecesEditor/components/PiecesEditorSheet/components/PieceEditorScreen/components/MovementsTab/MovementAddDialog.tsx
+++ app/src/features/variants/variantEditor/piecesEditor/components/PiecesEditorSheet/components/PieceEditorScreen/components/MovementsTab/MovementAddDialog.tsx
@@ -1,16 +1,53 @@
 import { Button } from "@/components/ui/button";
-import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog"
+import {
+	Dialog,
+	DialogContent,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+} from "@/components/ui/dialog";
+import useMovementAddDialogStore from "@/features/variants/variantEditor/piecesEditor/stores/movementAddDialog";
+import { Badge } from "@/components/ui/badge";
 
 function MovementAddDialog() {
+	const {
+		isMovementAddDialogOpen,
+		openMovementAddDialog,
+		closeMovementAddDialog,
+		movementsAdded,
+	} = useMovementAddDialogStore();
+
 	return (
-		<Dialog open={true}>
+		<Dialog
+			open={isMovementAddDialogOpen}
+			onOpenChange={(open) => {
+				if (open) {
+					openMovementAddDialog();
+				} else {
+					closeMovementAddDialog();
+				}
+			}}
+		>
 			<DialogContent>
 				<DialogHeader>
 					<DialogTitle>Add movements</DialogTitle>
 				</DialogHeader>
 
 				<div className="flex flex-col gap-2">
-					<p className="font-semibold text-muted-foreground">Movements</p>
+					<p className="font-semibold text-muted-foreground">
+						Movements
+					</p>
+
+					<div className="flex flex-row">
+						{movementsAdded.map((movementName) => (
+							<Badge
+								key={movementName}
+								variant="outline"
+							>
+								{movementName}
+							</Badge>
+						))}
+					</div>
 				</div>
 
 				<DialogFooter>
@@ -18,7 +55,7 @@ function MovementAddDialog() {
 				</DialogFooter>
 			</DialogContent>
 		</Dialog>
-	)
+	);
 }
 
 export default MovementAddDialog;
