319a269b01480a31c66f0b42f9905391c945cdd6
Teddi <teddikao@gmail.com>
2026-04-03 16:15:15 +0800

    Add additional state to track which piece is movement name selected for

--- app/src/features/variants/variantEditor/piecesEditor/stores/movementSelectionDialog.ts
+++ app/src/features/variants/variantEditor/piecesEditor/stores/movementSelectionDialog.ts
@@ -5,6 +5,10 @@ type MovementSelectionDialogStore = {
 	openMovementSelectionDialog: () => void;
 	closeMovementSelectionDialog: () => void;
 
+	pieceName: string | null;
+	updatePieceName: (pieceName: string) => void;
+	clearPieceName: () => void;
+
 	selectedMovements: string[];
 	selectMovement: (movement: string) => void;
 	deselectMovement: (movement: string) => void;
@@ -19,6 +23,10 @@ const useMovementSelectionDialogStore = create<MovementSelectionDialogStore>((se
 	isMovementSelectionDialogOpen: false,
 	openMovementSelectionDialog: () => set({ isMovementSelectionDialogOpen: true }),
 	closeMovementSelectionDialog: () => set({ isMovementSelectionDialogOpen: false }),
+
+	pieceName: null,
+	updatePieceName: (pieceName: string) => set({ pieceName }),
+	clearPieceName: () => set({ pieceName: null }),
 
 	selectedMovements: [],
 	selectMovement: (movement: string) => set((state) => ({ selectedMovements: [...state.selectedMovements, movement] })),
