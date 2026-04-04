585732919d60993ad8230733e310cf386470c15d
Teddi <teddikao@gmail.com>
2026-04-04 14:52:43 +0800

    Add logic to open add chained moves dialog on button lcick

--- app/src/features/variants/variantEditor/piecesEditor/components/PiecesEditorSheet/components/PieceEditorScreen/components/MovementsTab/ChainedMovesDialog.tsx
+++ app/src/features/variants/variantEditor/piecesEditor/components/PiecesEditorSheet/components/PieceEditorScreen/components/MovementsTab/ChainedMovesDialog.tsx
@@ -6,6 +6,8 @@ import {
 	DialogHeader,
 	DialogTitle,
 } from "@/components/ui/dialog";
+import AddChainedMoveDialog from "@/features/variants/variantEditor/piecesEditor/components/PiecesEditorSheet/components/PieceEditorScreen/components/MovementsTab/AddChainedMoveDialog";
+import useAddChainedMoveDialogStore from "@/features/variants/variantEditor/piecesEditor/stores/addChainedMoveDialog";
 import useChainedMovesDialogStore from "@/features/variants/variantEditor/piecesEditor/stores/chainedMovesDialog";
 import usePiecesEditorStore from "@/features/variants/variantEditor/piecesEditor/stores/piecesEditor";
 import { IconArrowRight, IconPlus } from "@tabler/icons-react";
@@ -19,60 +21,71 @@ function ChainedMovesDialog() {
 	} = useChainedMovesDialogStore();
 	const { chainedMoveSequences } = usePiecesEditorStore();
 
+	const { openChainedMoveDialog, updateChainedMoveSequenceIndex } = useAddChainedMoveDialogStore();
+
+	function handleAddChainedMoveButtonClick(chainedMoveSequenceIndex: number) {
+		updateChainedMoveSequenceIndex(chainedMoveSequenceIndex);
+		openChainedMoveDialog();
+	}
+
 	return (
-		<Dialog
-			open={isChainedMovesDialogOpen}
-			onOpenChange={(open) => {
-				if (open) {
-					openChainedMovesDialog();
-				} else {
-					closeChainedMovesDialog();
-					clearActivePiece();
-				}
-			}}
-		>
-			<DialogContent className="min-w-[50vw]">
-				<DialogHeader>
-					<DialogTitle>Chained moves</DialogTitle>
-					<DialogDescription>
-						View and modify chained move sequences
-					</DialogDescription>
-				</DialogHeader>
+		<>
+			<Dialog
+				open={isChainedMovesDialogOpen}
+				onOpenChange={(open) => {
+					if (open) {
+						openChainedMovesDialog();
+					} else {
+						closeChainedMovesDialog();
+						clearActivePiece();
+					}
+				}}
+			>
+				<DialogContent className="min-w-[50vw]">
+					<DialogHeader>
+						<DialogTitle>Chained moves</DialogTitle>
+						<DialogDescription>
+							View and modify chained move sequences
+						</DialogDescription>
+					</DialogHeader>
 
-				<div className="flex flex-col gap-2">
-					{chainedMoveSequences.map((sequence, index) => (
-						<div
-							className="flex flex-row items-center justify-between p-4 rounded-lg border-muted-foreground border-2 border-dashed"
-							key={index}
-						>
-							<div className="flex flex-row items-center">
-								{sequence.map((node, nodeIndex) => {
-									return (
-										<div
-											key={nodeIndex}
-											className="flex flex-row items-center"
-										>
-											<p className="px-4 py-2 rounded-md bg-muted">
-												{node.moveName}
-											</p>
+					<div className="flex flex-col gap-2">
+						{chainedMoveSequences.map((sequence, index) => (
+							<div
+								className="flex flex-row items-center justify-between p-4 rounded-lg border-muted-foreground border-2 border-dashed"
+								key={index}
+							>
+								<div className="flex flex-row items-center">
+									{sequence.map((node, nodeIndex) => {
+										return (
+											<div
+												key={nodeIndex}
+												className="flex flex-row items-center"
+											>
+												<p className="px-4 py-2 rounded-md bg-muted">
+													{node.moveName}
+												</p>
 
-											{nodeIndex <
-												sequence.length - 1 && (
-												<IconArrowRight />
-											)}
-										</div>
-									);
-								})}
+												{nodeIndex <
+													sequence.length - 1 && (
+													<IconArrowRight />
+												)}
+											</div>
+										);
+									})}
+								</div>
+
+								<Button onClick={() => handleAddChainedMoveButtonClick(index)} variant="outline" size="icon-sm">
+									<IconPlus />
+								</Button>
 							</div>
+						))}
+					</div>
+				</DialogContent>
+			</Dialog>
 
-							<Button variant="outline" size="icon-sm">
-								<IconPlus />
-							</Button>
-						</div>
-					))}
-				</div>
-			</DialogContent>
-		</Dialog>
+			<AddChainedMoveDialog />
+		</>
 	);
 }
 
