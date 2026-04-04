f2fa7653786e30fc65579d40de1c80b5d9bcc40d
Teddi <teddikao@gmail.com>
2026-04-04 11:08:27 +0800

    Attach open and onOpenChange handlers to dialog

--- app/src/features/variants/variantEditor/piecesEditor/components/PiecesEditorSheet/components/PieceEditorScreen/components/MovementsTab/ChainedMovesDialog.tsx
+++ app/src/features/variants/variantEditor/piecesEditor/components/PiecesEditorSheet/components/PieceEditorScreen/components/MovementsTab/ChainedMovesDialog.tsx
@@ -1,8 +1,18 @@
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import useChainedMovesDialogStore from "@/features/variants/variantEditor/piecesEditor/stores/chainedMovesDialog";
 
 function ChainedMovesDialog() {
+	const { isChainedMovesDialogOpen, openChainedMovesDialog, closeChainedMovesDialog, clearActivePiece } = useChainedMovesDialogStore();
+
 	return (
-		<Dialog>
+		<Dialog open={isChainedMovesDialogOpen} onOpenChange={(open) => {
+			if (open) {
+				openChainedMovesDialog();
+			} else {
+				closeChainedMovesDialog();
+				clearActivePiece();
+			}
+		}}>
 			<DialogContent>
 				<DialogHeader>
 					<DialogTitle>Chained moves</DialogTitle>
