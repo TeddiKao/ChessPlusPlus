6484907b0f65fe289200e1358989446e2b60c511
Teddi <teddikao@gmail.com>
2026-04-04 11:06:12 +0800

    Add active piece state to dialog store

--- app/src/features/variants/variantEditor/piecesEditor/stores/chainedMovesDialog.ts
+++ app/src/features/variants/variantEditor/piecesEditor/stores/chainedMovesDialog.ts
@@ -4,12 +4,20 @@ type ChainedMovesDialogStore = {
 	isChainedMovesDialogOpen: boolean;
 	openChainedMovesDialog: () => void;
 	closeChainedMovesDialog: () => void;
+
+	activePiece: string | null;
+	updateActivePiece: (piece: string) => void;
+	clearActivePiece: () => void;
 };
 
 const useChainedMovesDialogStore = create<ChainedMovesDialogStore>((set) => ({
 	isChainedMovesDialogOpen: false,
 	openChainedMovesDialog: () => set({ isChainedMovesDialogOpen: true }),
 	closeChainedMovesDialog: () => set({ isChainedMovesDialogOpen: false }),
+
+	activePiece: null,
+	updateActivePiece: (piece) => set({ activePiece: piece }),
+	clearActivePiece: () => set({ activePiece: null }),
 }));
 
 export default useChainedMovesDialogStore;
