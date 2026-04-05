90267ee9d6c98d55751905bd9aa799ad81a54120
Teddi <teddikao@gmail.com>
2026-04-05 11:55:51 +0800

    Add additonal state to track selected movements

--- app/src/features/variants/variantEditor/piecesEditor/stores/chainedMoveSequenceCreationDialog.ts
+++ app/src/features/variants/variantEditor/piecesEditor/stores/chainedMoveSequenceCreationDialog.ts
@@ -8,16 +8,37 @@ type ChainedMoveSequenceCreationDialogStore = {
 	searchQuery: string;
 	updateSearchQuery: (query: string) => void;
 	clearSearchQuery: () => void;
+
+	selectedMovements: [number, string][];
+	selectMovement: (movementName: string) => void;
+	deselectMovement: (movementName: string) => void;
+	clearSelectedMovements: () => void;
 };
 
-const useChainedMoveSequenceCreationDialogStore = create<ChainedMoveSequenceCreationDialogStore>((set) => ({
-	isChainedMoveSequenceCreationDialogOpen: false,
-	openChainedMoveSequenceCreationDialog: () => set({ isChainedMoveSequenceCreationDialogOpen: true }),
-	closeChainedMoveSequenceCreationDialog: () => set({ isChainedMoveSequenceCreationDialogOpen: false }),
+const useChainedMoveSequenceCreationDialogStore =
+	create<ChainedMoveSequenceCreationDialogStore>((set) => ({
+		isChainedMoveSequenceCreationDialogOpen: false,
+		openChainedMoveSequenceCreationDialog: () =>
+			set({ isChainedMoveSequenceCreationDialogOpen: true }),
+		closeChainedMoveSequenceCreationDialog: () =>
+			set({ isChainedMoveSequenceCreationDialogOpen: false }),
 
-	searchQuery: "",
-	updateSearchQuery: (query) => set({ searchQuery: query }),
-	clearSearchQuery: () => set({ searchQuery: "" }),
-}));
+		searchQuery: "",
+		updateSearchQuery: (query) => set({ searchQuery: query }),
+		clearSearchQuery: () => set({ searchQuery: "" }),
+
+		selectedMovements: [],
+		selectMovement: (movementName) =>
+			set((state) => ({
+				selectedMovements: [...state.selectedMovements, [state.selectedMovements.length, movementName]],
+			})),
+		deselectMovement: (movementName) =>
+			set((state) => ({
+				selectedMovements: state.selectedMovements.filter(
+					(m) => m[1] !== movementName,
+				),
+			})),
+		clearSelectedMovements: () => set({ selectedMovements: [] }),
+	}));
 
 export default useChainedMoveSequenceCreationDialogStore;
