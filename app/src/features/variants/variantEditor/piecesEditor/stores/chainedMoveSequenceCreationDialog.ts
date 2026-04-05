ac87e4227c048d99f61e3aadad86f0d41b33ce00
Teddi <teddikao@gmail.com>
2026-04-05 11:53:03 +0800

    Add state to track search query in sequence creation dialog

--- app/src/features/variants/variantEditor/piecesEditor/stores/chainedMoveSequenceCreationDialog.ts
+++ app/src/features/variants/variantEditor/piecesEditor/stores/chainedMoveSequenceCreationDialog.ts
@@ -4,12 +4,20 @@ type ChainedMoveSequenceCreationDialogStore = {
 	isChainedMoveSequenceCreationDialogOpen: boolean;
 	openChainedMoveSequenceCreationDialog: () => void;
 	closeChainedMoveSequenceCreationDialog: () => void;
+
+	searchQuery: string;
+	updateSearchQuery: (query: string) => void;
+	clearSearchQuery: () => void;
 };
 
 const useChainedMoveSequenceCreationDialogStore = create<ChainedMoveSequenceCreationDialogStore>((set) => ({
 	isChainedMoveSequenceCreationDialogOpen: false,
 	openChainedMoveSequenceCreationDialog: () => set({ isChainedMoveSequenceCreationDialogOpen: true }),
 	closeChainedMoveSequenceCreationDialog: () => set({ isChainedMoveSequenceCreationDialogOpen: false }),
+
+	searchQuery: "",
+	updateSearchQuery: (query) => set({ searchQuery: query }),
+	clearSearchQuery: () => set({ searchQuery: "" }),
 }));
 
 export default useChainedMoveSequenceCreationDialogStore;
