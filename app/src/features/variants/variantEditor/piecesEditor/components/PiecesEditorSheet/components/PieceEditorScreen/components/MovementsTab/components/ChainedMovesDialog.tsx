10941d58c9b40834a1a0bf386d26967408ef7622
Teddi <teddikao@gmail.com>
2026-04-05 14:10:47 +0800

    Add dropdown menu items related to move deletion

--- app/src/features/variants/variantEditor/piecesEditor/components/PiecesEditorSheet/components/PieceEditorScreen/components/MovementsTab/components/ChainedMovesDialog.tsx
+++ app/src/features/variants/variantEditor/piecesEditor/components/PiecesEditorSheet/components/PieceEditorScreen/components/MovementsTab/components/ChainedMovesDialog.tsx
@@ -6,6 +6,12 @@ import {
 	DialogHeader,
 	DialogTitle,
 } from "@/components/ui/dialog";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import useVariantDraftStore from "@/features/variants/variantEditor/common/stores/variantDraft";
 import AddChainedMoveDialog from "@/features/variants/variantEditor/piecesEditor/components/PiecesEditorSheet/components/PieceEditorScreen/components/MovementsTab/components/AddChainedMoveDialog";
@@ -23,15 +29,19 @@ function ChainedMovesDialog() {
 		openChainedMovesDialog,
 		closeChainedMovesDialog,
 		activePiece,
-		clearActivePiece
+		clearActivePiece,
 	} = useChainedMovesDialogStore();
 	const { chainedMoveSequences } = usePiecesEditorStore();
-	const { pieceRulesetDraft, updatePieceRulesetDraft, syncPieceRulesetDraftToDB } =
-		useVariantDraftStore();
+	const {
+		pieceRulesetDraft,
+		updatePieceRulesetDraft,
+		syncPieceRulesetDraftToDB,
+	} = useVariantDraftStore();
 
 	const { openChainedMoveDialog, updateChainedMoveSequenceIndex } =
 		useAddChainedMoveDialogStore();
-	const { openChainedMoveSequenceCreationDialog } = useChainedMoveSequenceCreationDialogStore();
+	const { openChainedMoveSequenceCreationDialog } =
+		useChainedMoveSequenceCreationDialogStore();
 
 	function handleAddChainedMoveButtonClick(chainedMoveSequenceIndex: number) {
 		updateChainedMoveSequenceIndex(chainedMoveSequenceIndex);
@@ -42,14 +52,15 @@ function ChainedMovesDialog() {
 		if (!pieceRulesetDraft) return;
 		if (!activePiece) return;
 
-		const updatedPieceRulesetDraft = structuredClone(pieceRulesetDraft);;
+		const updatedPieceRulesetDraft = structuredClone(pieceRulesetDraft);
 
 		chainedMoveSequences.forEach((sequence) => {
 			const indexToUpdate = sequence[0];
 			if (isNullOrUndefined(indexToUpdate)) {
 				updatedPieceRulesetDraft[activePiece].moveset.push(sequence[1]);
 			} else {
-				updatedPieceRulesetDraft[activePiece].moveset[indexToUpdate] = sequence[1];
+				updatedPieceRulesetDraft[activePiece].moveset[indexToUpdate] =
+					sequence[1];
 			}
 		});
 
@@ -92,27 +103,59 @@ function ChainedMovesDialog() {
 								<div className="flex min-w-0 flex-1 flex-row items-center justify-between rounded-lg border-2 border-dashed border-muted-foreground">
 									<ScrollArea className="min-w-0 w-full">
 										<div className="flex min-w-0 flex-row items-center p-4">
-											{sequence[1].map((node, nodeIndex) => {
-												return (
-													<div
-														key={nodeIndex}
-														className="flex flex-row items-center"
-													>
-														<Button
-															variant="ghost"
-															className="px-4 py-2 rounded-md bg-muted"
+											{sequence[1].map(
+												(node, nodeIndex) => {
+													return (
+														<div
+															key={nodeIndex}
+															className="flex flex-row items-center"
 														>
-															{node.moveName}
-														</Button>
+															<DropdownMenu>
+																<DropdownMenuTrigger
+																	asChild
+																>
+																	<Button
+																		variant="ghost"
+																		className="px-4 py-2 rounded-md bg-muted"
+																	>
+																		{
+																			node.moveName
+																		}
+																	</Button>
 
-														{nodeIndex <
-															sequence[1].length -
-																1 && (
-															<IconArrowRight />
-														)}
-													</div>
-												);
-											})}
+																	{nodeIndex <
+																		sequence[1]
+																			.length -
+																			1 && (
+																		<IconArrowRight />
+																	)}
+																</DropdownMenuTrigger>
+
+																<DropdownMenuContent>
+																	<DropdownMenuItem variant="destructive">
+																		Delete
+																		move
+																	</DropdownMenuItem>
+																	<DropdownMenuItem variant="destructive">
+																		Delete
+																		moves
+																		before
+																	</DropdownMenuItem>
+																	<DropdownMenuItem variant="destructive">
+																		Delete
+																		moves
+																		after
+																	</DropdownMenuItem>
+																	<DropdownMenuItem variant="destructive">
+																		Delete
+																		sequence
+																	</DropdownMenuItem>
+																</DropdownMenuContent>
+															</DropdownMenu>
+														</div>
+													);
+												},
+											)}
 										</div>
 
 										<ScrollBar orientation="horizontal" />
@@ -131,7 +174,12 @@ function ChainedMovesDialog() {
 							</div>
 						))}
 
-						<Button onClick={handleAddSequenceButtonClick} className="w-full">Add sequence</Button>
+						<Button
+							onClick={handleAddSequenceButtonClick}
+							className="w-full"
+						>
+							Add sequence
+						</Button>
 					</div>
 				</DialogContent>
 			</Dialog>
