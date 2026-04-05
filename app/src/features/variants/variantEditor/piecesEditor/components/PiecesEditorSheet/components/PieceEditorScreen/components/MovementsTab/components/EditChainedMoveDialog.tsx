7d1f0634b1187abad4817398122b8c034ae5e42e
Teddi <teddikao@gmail.com>
2026-04-05 18:29:33 +0800

    Add header markup for editing chained move dialog

---
+++ app/src/features/variants/variantEditor/piecesEditor/components/PiecesEditorSheet/components/PieceEditorScreen/components/MovementsTab/components/EditChainedMoveDialog.tsx
@@ -0,0 +1,15 @@
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+
+function EditChainedMoveDialog() {
+	return (
+		<Dialog>
+			<DialogContent>
+				<DialogHeader>
+					<DialogTitle>Edit chained move</DialogTitle>
+				</DialogHeader>
+			</DialogContent>
+		</Dialog>
+	)
+}
+
+export default EditChainedMoveDialog;
