5698e0b69f404b2151d4574c0bed62b3a2b94afe
Teddi <teddikao@gmail.com>
2026-04-05 18:36:20 +0800

    Add open and onOpenChange handlers

--- app/src/features/variants/variantEditor/piecesEditor/components/PiecesEditorSheet/components/PieceEditorScreen/components/MovementsTab/components/EditChainedMoveDialog.tsx
+++ app/src/features/variants/variantEditor/piecesEditor/components/PiecesEditorSheet/components/PieceEditorScreen/components/MovementsTab/components/EditChainedMoveDialog.tsx
@@ -1,15 +1,36 @@
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import {
+	Dialog,
+	DialogContent,
+	DialogHeader,
+	DialogTitle,
+} from "@/components/ui/dialog";
+import useEditChainedMoveDialogStore from "@/features/variants/variantEditor/piecesEditor/stores/editChainedMoveDialog";
 
 function EditChainedMoveDialog() {
+	const {
+		isEditChainedMoveDialogOpen,
+		openEditChainedMoveDialog,
+		closeEditChainedMoveDialog,
+	} = useEditChainedMoveDialogStore();
+
 	return (
-		<Dialog>
+		<Dialog
+			open={isEditChainedMoveDialogOpen}
+			onOpenChange={(open) => {
+				if (open) {
+					openEditChainedMoveDialog();
+				} else {
+					closeEditChainedMoveDialog();
+				}
+			}}
+		>
 			<DialogContent>
 				<DialogHeader>
 					<DialogTitle>Edit chained move</DialogTitle>
 				</DialogHeader>
 			</DialogContent>
 		</Dialog>
-	)
+	);
 }
 
 export default EditChainedMoveDialog;
