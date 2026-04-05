f0275fdd78feb3c2fa329453f2cbd0a36740a7a9
Teddi <teddikao@gmail.com>
2026-04-05 18:38:55 +0800

    Add combobox for selecting new move

--- app/src/features/variants/variantEditor/piecesEditor/components/PiecesEditorSheet/components/PieceEditorScreen/components/MovementsTab/components/EditChainedMoveDialog.tsx
+++ app/src/features/variants/variantEditor/piecesEditor/components/PiecesEditorSheet/components/PieceEditorScreen/components/MovementsTab/components/EditChainedMoveDialog.tsx
@@ -1,17 +1,40 @@
+import { Combobox, ComboboxContent, ComboboxEmpty, ComboboxInput, ComboboxItem, ComboboxList } from "@/components/ui/combobox";
 import {
 	Dialog,
 	DialogContent,
 	DialogHeader,
 	DialogTitle,
 } from "@/components/ui/dialog";
+import { Field, FieldLabel } from "@/components/ui/field";
+import type { MovementRule } from "@/features/variants/common/types/movementRules";
+import useVariantDraftStore from "@/features/variants/variantEditor/common/stores/variantDraft";
 import useEditChainedMoveDialogStore from "@/features/variants/variantEditor/piecesEditor/stores/editChainedMoveDialog";
+import type { ChangeEvent } from "react";
 
 function EditChainedMoveDialog() {
 	const {
 		isEditChainedMoveDialogOpen,
 		openEditChainedMoveDialog,
 		closeEditChainedMoveDialog,
+
+		newMovementName,
+		updateNewMovementName,
+		clearNewMovementName,
+
+		clearSequenceIndex,
+		clearNodeIndex,
 	} = useEditChainedMoveDialogStore();
+
+	const { movementRulesDraft } = useVariantDraftStore();
+	if (!movementRulesDraft) return null;
+
+	function handleMovementNameInputChange(e: ChangeEvent<HTMLInputElement>) {
+		updateNewMovementName(e.target.value);
+	}
+
+	function handleComboboxItemSelect(movementName: string) {
+		updateNewMovementName(movementName);
+	}
 
 	return (
 		<Dialog
@@ -21,6 +44,9 @@ function EditChainedMoveDialog() {
 					openEditChainedMoveDialog();
 				} else {
 					closeEditChainedMoveDialog();
+					clearNewMovementName();
+					clearSequenceIndex();
+					clearNodeIndex();
 				}
 			}}
 		>
@@ -28,6 +54,47 @@ function EditChainedMoveDialog() {
 				<DialogHeader>
 					<DialogTitle>Edit chained move</DialogTitle>
 				</DialogHeader>
+
+				<Field>
+					<FieldLabel htmlFor="movementNameComboboxInput">
+						Movement name
+					</FieldLabel>
+					<Combobox
+						id="movementNameComboboxInput"
+						modal={true}
+						items={Object.entries(movementRulesDraft)}
+						itemToStringValue={(
+							movementEntry: [string, MovementRule],
+						) => movementEntry[0]}
+					>
+						<ComboboxInput
+							value={newMovementName}
+							onChange={handleMovementNameInputChange}
+							placeholder="Select a move"
+						/>
+						<ComboboxContent
+							onWheel={(e) => e.stopPropagation()}
+							className="pointer-events-auto"
+						>
+							<ComboboxEmpty>No movements found</ComboboxEmpty>
+							<ComboboxList>
+								{(movementEntry: [string, MovementRule]) => (
+									<ComboboxItem
+										onClick={() =>
+											handleComboboxItemSelect(
+												movementEntry[0],
+											)
+										}
+										key={movementEntry[0]}
+										value={movementEntry[0]}
+									>
+										{movementEntry[0]}
+									</ComboboxItem>
+								)}
+							</ComboboxList>
+						</ComboboxContent>
+					</Combobox>
+				</Field>
 			</DialogContent>
 		</Dialog>
 	);
