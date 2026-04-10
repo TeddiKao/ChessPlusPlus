9d58ec30e89d455133e8737125e0af75781511dc
Teddi <teddikao@gmail.com>
2026-04-10 09:58:16 +0800

    Create schemas for legal move generation

---
+++ backend/app/schemas/legal_move_generator_request.py
@@ -0,0 +1,9 @@
+from app.schemas.base_schema import BaseSchema
+
+class GenerateLegalMovesRequest(BaseSchema):
+    current_pos: tuple[int, int]
+    game_state: dict
+    piece_ruleset: dict
+
+class GenerateLegalMovesResponse(BaseSchema):
+    legal_moves: dict[str, list[tuple[int, int]]]
