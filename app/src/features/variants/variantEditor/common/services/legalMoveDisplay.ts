6996d92928b073e7956be5fe4e22f76ab049e417
Teddi <teddikao@gmail.com>
2026-04-24 21:11:29 +0800

    Create frontend service function to send request to backend to generate legal moves

--- app/src/features/variants/variantEditor/common/services/legalMoveDisplay.ts
+++ app/src/features/variants/variantEditor/common/services/legalMoveDisplay.ts
@@ -1,5 +1,32 @@
-async function displayLegalMoves() {
+import api from "@/app/api";
+import type { MovementRules } from "@/features/variants/common/types/movementRules";
+import type { PieceRuleset } from "@/features/variants/common/types/pieceRules";
 
+type LegalMoveDisplayRequestBody = {
+    pieceName: string;
+    currentPos: [number, number];
+    gameState: [[number, number], string];
+    pieceRuleset: PieceRuleset;
+    movementRules: MovementRules;
+}
+
+type LegalMoveDisplayResponseBody = Record<string, [number, number][]>;
+
+async function displayLegalMoves(request: LegalMoveDisplayRequestBody): Promise<LegalMoveDisplayResponseBody> {
+    try {
+        const response = await api.post("generate-legal-moves", {
+            pieceName: request.pieceName,
+            currentPos: request.currentPos,
+            gameState: request.gameState,
+            pieceRuleset: request.pieceRuleset,
+            movementRules: request.movementRules
+        })
+
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        return {};
+    }
 }
 
 export { displayLegalMoves };
