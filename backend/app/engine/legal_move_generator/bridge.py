2dfdbc5666547604fe2ac0adf8a82e73b6022963
Teddi <teddikao@gmail.com>
2026-04-24 21:45:45 +0800

    Enhance bridge function to increse flexibility

--- backend/app/engine/legal_move_generator/bridge.py
+++ backend/app/engine/legal_move_generator/bridge.py
@@ -14,18 +14,23 @@ def convert_piece_json_ruleset_to_piece_object(piece_json_ruleset: dict):
         data=piece_json_ruleset["data"]
     )
 
-def generate_legal_moves_for_preview(piece_name: str, current_pos: tuple[int, int], game_state: dict, piece_ruleset: dict, movement_rules: dict):
+def generate_legal_moves_for_preview(piece_name: str, current_pos: tuple[int, int], game_state: list, piece_ruleset: dict, movement_rules: dict):
     serialized_piece_ruleset = convert_camel_to_snake(piece_ruleset, [1, 2])
     serialized_movement_rules = convert_camel_to_snake(movement_rules, [1])
     
     game_instance = Game({ "pieces": serialized_piece_ruleset, "moves": serialized_movement_rules })
-    
-    game_state = {}
-    game_state[current_pos] = Piece(
-        position=current_pos,
-        piece_id=0,
-        piece_name=piece_name,
-        data={ "has_not_moved": True }
-    )
+
+    serialised_game_state = dict(game_state)
+
+    id_counter = 0
+    for square, piece_name in serialised_game_state.items():
+        game_instance._game_state[square] = Piece(
+            position=square,
+            piece_id=id_counter,
+            piece_name=piece_name,
+            data={ "has_not_moved": True }
+        )
+
+        id_counter += 1
 
     return game_instance.get_legal_moves(current_pos)
