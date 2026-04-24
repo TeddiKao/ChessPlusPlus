bd53e467f75c267f7b364fffcfc8353ee2cff15c
Teddi <teddikao@gmail.com>
2026-04-24 21:50:36 +0800

    Wrap app with query client provider

--- app/src/app/App.tsx
+++ app/src/app/App.tsx
@@ -6,25 +6,30 @@ import JSONValidatorTestPage from "@/pages/JSONValidatorTestPage";
 import VariantEditorPage from "@/pages/VariantEditorPage";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import HomePage from "@/pages/HomePage";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
 	return (
-		<TooltipProvider>
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<HomePage />} />
-					<Route
-						path="/variants/:variantId"
-						element={<VariantEditorPage />}
-					/>
-					<Route path="/test" element={<TestPage />} />
-					<Route
-						path="/json-validator-test"
-						element={<JSONValidatorTestPage />}
-					/>
-				</Routes>
-			</BrowserRouter>
-		</TooltipProvider>
+		<QueryClientProvider client={queryClient}>
+			<TooltipProvider>
+				<BrowserRouter>
+					<Routes>
+						<Route path="/" element={<HomePage />} />
+						<Route
+							path="/variants/:variantId"
+							element={<VariantEditorPage />}
+						/>
+						<Route path="/test" element={<TestPage />} />
+						<Route
+							path="/json-validator-test"
+							element={<JSONValidatorTestPage />}
+						/>
+					</Routes>
+				</BrowserRouter>
+			</TooltipProvider>
+		</QueryClientProvider>
 	);
 }
 
