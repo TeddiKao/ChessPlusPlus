ab142261442ee292403bdcb54970f85e9583edd7
Teddi <teddikao@gmail.com>
2026-04-24 21:04:46 +0800

    Add logic to serialize pieces and generate legal moves

--- backend/app/engine/legal_move_generator/bridge.py
+++ backend/app/engine/legal_move_generator/bridge.py
@@ -1,5 +1,7 @@
+from app.engine.legal_move_generator.legal_move_generator import Game
 from typing import TypedDict
 from app.engine.legal_move_generator.legal_move_generator import Piece
+from app.utils.case_converter import convert_camel_to_snake
 
 class PieceJSONRuleset(TypedDict):
 	pos: tuple[int, int]
@@ -11,3 +13,19 @@ def convert_piece_json_ruleset_to_piece_object(piece_json_ruleset: dict):
         piece_name=piece_json_ruleset["piece_name"],
         data=piece_json_ruleset["data"]
     )
+
+def generate_legal_moves_for_preview(piece_name: str, current_pos: tuple[int, int], game_state: dict, piece_ruleset: dict, movement_rules: dict):
+    serialized_piece_ruleset = convert_camel_to_snake(piece_ruleset, [1, 2])
+    serialized_movement_rules = convert_camel_to_snake(movement_rules, [1])
+    
+    game_instance = Game({ "pieces": serialized_piece_ruleset, "moves": serialized_movement_rules })
+    
+    game_state = {}
+    game_state[current_pos] = Piece(
+        position=current_pos,
+        piece_id=0,
+        piece_name=piece_name,
+        data={ "has_not_moved": True }
+    )
+
+    return game_instance.get_legal_moves(current_pos)
