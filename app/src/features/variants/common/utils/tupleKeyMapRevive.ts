f55839f074cfedfffcd969fd8b28e5278dd52fab
Teddi <teddikao@gmail.com>
2026-04-25 22:21:01 +0800

    Remove console.log statement

--- app/src/features/variants/common/utils/tupleKeyMapRevive.ts
+++ app/src/features/variants/common/utils/tupleKeyMapRevive.ts
@@ -7,7 +7,6 @@ function reviveTupleKeyedMap<K extends readonly unknown[], V>(mapToRevive: unkno
     const revivedMap = new TupleKeyedMap<K, V>();
 
     function walkMap(map: Map<unknown, unknown>, prefix: readonly unknown[] = []) {
-        console.log(map);
         for (const [key, value] of Array.from(map)) {
             const mapKey = [...prefix, key] as unknown as K;
             if (value instanceof Map) {
