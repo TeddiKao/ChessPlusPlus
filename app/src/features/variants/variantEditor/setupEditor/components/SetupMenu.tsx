6b98365b829728dc9d43b40ee0bc8e554da81a25
Teddi <teddikao@gmail.com>
2026-05-08 20:44:23 +0800

    Add piece image display logic

--- app/src/features/variants/variantEditor/setupEditor/components/SetupMenu.tsx
+++ app/src/features/variants/variantEditor/setupEditor/components/SetupMenu.tsx
@@ -4,14 +4,20 @@ import {
 	CollapsibleContent,
 	CollapsibleTrigger,
 } from "@/components/ui/collapsible";
-import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import usePieceImagesStore from "@/features/variants/common/stores/pieceImages";
 import useVariantDraftStore from "@/features/variants/variantEditor/common/stores/variantDraft";
 import { IconChevronDown, IconX } from "@tabler/icons-react";
 
 function SetupMenu() {
-	const { setupRulesDraft } = useVariantDraftStore();
+	const { setupRulesDraft, pieceRulesetDraft, currentVariantId } =
+		useVariantDraftStore();
+	const { images } = usePieceImagesStore();
 
 	if (!setupRulesDraft) return null;
+	if (!pieceRulesetDraft) return null;
+	if (!images) return null;
+	if (!currentVariantId) return null;
 
 	const pieceOwnershipRules = setupRulesDraft.pieceOwnership;
 	const colorsPlaying = Object.keys(pieceOwnershipRules);
@@ -48,6 +54,27 @@ function SetupMenu() {
 								</TabsTrigger>
 							))}
 						</TabsList>
+
+						{colorsPlaying.map((color) => (
+							<TabsContent key={color} value={color}>
+								<div className="flex flex-row items-center justify-between w-full p-2">
+									{pieceOwnershipRules[
+										color as keyof typeof pieceOwnershipRules
+									].map((piece) => (
+										<img
+											key={piece}
+											className="size-12"
+											src={URL.createObjectURL(
+												images[piece][
+													currentVariantId
+												] ?? images[piece].image,
+											)}
+											alt={piece}
+										/>
+									))}
+								</div>
+							</TabsContent>
+						))}
 					</Tabs>
 				</CollapsibleContent>
 			</Collapsible>
