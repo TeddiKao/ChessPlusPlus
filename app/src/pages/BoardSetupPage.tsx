0755978e18b5fe40f2f3354211fe8c1d145ed55f
Teddi <teddikao@gmail.com>
2026-05-09 21:54:59 +0800

    Add functionality to update draft state after drag is completed

--- app/src/pages/BoardSetupPage.tsx
+++ app/src/pages/BoardSetupPage.tsx
@@ -1,19 +1,44 @@
+import useVariantDraftStore from "@/features/variants/variantEditor/common/stores/variantDraft";
 import SetupChessboard from "@/features/variants/variantEditor/setupEditor/components/SetupChessboard/SetupChessboard";
 import SetupMenu from "@/features/variants/variantEditor/setupEditor/components/SetupMenu";
 import SetupToolbar from "@/features/variants/variantEditor/setupEditor/components/SetupToolbar";
-import {
-	DragDropProvider,
-} from "@dnd-kit/react";
+import { DragDropProvider } from "@dnd-kit/react";
 
 type OnDragEnd = React.ComponentProps<typeof DragDropProvider>["onDragEnd"];
 
 function BoardSetupPage() {
+	const { setupRulesDraft, updateSetupRulesDraft } = useVariantDraftStore();
+	if (!setupRulesDraft) return null;
+
 	function handleDragEnd(...args: Parameters<NonNullable<OnDragEnd>>) {
+		if (!setupRulesDraft) return;
+
 		const [event] = args;
 
 		if (event.operation.canceled) return;
 
-		console.log(event.operation.target?.id);
+		const targetSquareId = event.operation.target?.id;
+		const [file, rank] = (targetSquareId as string)?.split("-") ?? [];
+
+		if (!file) return;
+		if (!rank) return;
+
+		const [player, piece] =
+			(event.operation.source?.id as string).split("-") ?? [];
+
+		if (!player) return;
+		if (!piece) return;
+
+		const updatedSetupRulesDraft = structuredClone(setupRulesDraft);
+		updatedSetupRulesDraft.startingPosition = [
+			...updatedSetupRulesDraft.startingPosition,
+			[[Number(file), Number(rank)], piece],
+		];
+
+		updateSetupRulesDraft({
+			...setupRulesDraft,
+			startingPosition: updatedSetupRulesDraft.startingPosition,
+		});
 	}
 
 	return (
