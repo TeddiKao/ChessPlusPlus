5df796b05f6b3fb6eeefa05636fb8507b05cf7ce
Teddi <teddikao@gmail.com>
2026-05-09 18:35:45 +0800

    Wrap entire setup page in drag drop provider

--- app/src/pages/BoardSetupPage.tsx
+++ app/src/pages/BoardSetupPage.tsx
@@ -1,15 +1,18 @@
 import SetupChessboard from "@/features/variants/variantEditor/setupEditor/components/SetupChessboard";
 import SetupMenu from "@/features/variants/variantEditor/setupEditor/components/SetupMenu";
 import SetupToolbar from "@/features/variants/variantEditor/setupEditor/components/SetupToolbar";
+import { DragDropProvider } from "@dnd-kit/react";
 
 function BoardSetupPage() {
 	return (
 		<div className="flex flex-row items-center justify-center w-full h-full">
-			<div className="flex flex-row w-full h-full items-center justify-center gap-4">
-				<SetupChessboard />
-				<SetupToolbar />
-				<SetupMenu />
-			</div>
+			<DragDropProvider>
+				<div className="flex flex-row w-full h-full items-center justify-center gap-4">
+					<SetupChessboard />
+					<SetupToolbar />
+					<SetupMenu />
+				</div>
+			</DragDropProvider>
 		</div>
 	);
 }
