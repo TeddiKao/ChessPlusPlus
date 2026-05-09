289d4542e3ba53fdc7f4a603d65217c01618c501
Teddi <teddikao@gmail.com>
2026-05-09 22:11:07 +0800

    Add identifiers to ensure piece image is unique

--- app/src/features/variants/variantEditor/setupEditor/components/SetupChessboard/PieceImage.tsx
+++ app/src/features/variants/variantEditor/setupEditor/components/SetupChessboard/PieceImage.tsx
@@ -1,5 +1,18 @@
-function PieceImage() {
+import { useDraggable } from "@dnd-kit/react";
 
+type PieceImageProps = {
+	imageUrl: string;
+	piece: string;
+	file: number;
+	rank: number;
+};
+
+function PieceImage({ piece, imageUrl, file, rank }: PieceImageProps) {
+	const { ref } = useDraggable({
+		id: `${file}_${rank}-${piece}`,
+	});
+
+	return <img ref={ref} src={imageUrl} alt={piece} />;
 }
 
 export default PieceImage;
