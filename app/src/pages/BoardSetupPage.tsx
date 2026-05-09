b6a0c70bbc9719dd02a1e909c0b1444436f341ee
Teddi <teddikao@gmail.com>
2026-05-09 21:45:49 +0800

    Add test code for drag end detection

--- app/src/pages/BoardSetupPage.tsx
+++ app/src/pages/BoardSetupPage.tsx
@@ -1,12 +1,24 @@
 import SetupChessboard from "@/features/variants/variantEditor/setupEditor/components/SetupChessboard/SetupChessboard";
 import SetupMenu from "@/features/variants/variantEditor/setupEditor/components/SetupMenu";
 import SetupToolbar from "@/features/variants/variantEditor/setupEditor/components/SetupToolbar";
-import { DragDropProvider } from "@dnd-kit/react";
+import {
+	DragDropProvider,
+} from "@dnd-kit/react";
+
+type OnDragEnd = React.ComponentProps<typeof DragDropProvider>["onDragEnd"];
 
 function BoardSetupPage() {
+	function handleDragEnd(...args: Parameters<NonNullable<OnDragEnd>>) {
+		const [event] = args;
+
+		if (event.operation.canceled) return;
+
+		console.log(event.operation.target?.id);
+	}
+
 	return (
 		<div className="flex flex-row items-center justify-center w-full h-full">
-			<DragDropProvider>
+			<DragDropProvider onDragEnd={handleDragEnd}>
 				<div className="flex flex-row w-full h-full items-center justify-center gap-4">
 					<SetupChessboard />
 					<SetupToolbar />
