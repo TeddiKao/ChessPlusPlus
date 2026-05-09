d96a23c0b9f55cbe9bba5575b2ea735b69240651
Teddi <teddikao@gmail.com>
2026-05-09 21:23:29 +0800

    Create square component to support adding of droppable

--- app/src/features/variants/variantEditor/setupEditor/components/SetupChessboard/Square.tsx
+++ app/src/features/variants/variantEditor/setupEditor/components/SetupChessboard/Square.tsx
@@ -1,10 +1,20 @@
 type SquareProps = {
 	file: number;
 	rank: number;
+	imageComponent: React.ReactNode;
 };
 
-function Square({ file, rank }: SquareProps) {
+function Square({ file, rank, imageComponent }: SquareProps) {
+	const isDark = (rank + file) % 2 === 0;
 
+	return (
+		<div
+			key={`${file}-${rank}`}
+			className={`${isDark ? "bg-chessboard-square-dark" : "bg-chessboard-square-light"} aspect-square relative`}
+		>
+			{imageComponent}
+		</div>
+	);
 }
 
 export default Square;
