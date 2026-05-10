930575e341d7357816634ecef506ae5546a117e9
Teddi <teddikao@gmail.com>
2026-05-10 16:22:43 +0800

    Add styling to display coordinates

--- app/src/features/variants/variantEditor/setupEditor/components/SetupChessboard/Square.tsx
+++ app/src/features/variants/variantEditor/setupEditor/components/SetupChessboard/Square.tsx
@@ -1,5 +1,6 @@
 import PieceImage from "@/features/variants/variantEditor/setupEditor/components/SetupChessboard/PieceImage";
 import { useDroppable } from "@dnd-kit/react";
+import clsx from "clsx";
 
 type SquareProps = {
 	file: number;
@@ -13,7 +14,6 @@ function Square({ file, rank, imageUrl, piece }: SquareProps) {
 		id: `${file}-${rank}`,
 	});
 
-
 	const isDark = (rank + file) % 2 === 0;
 
 	return (
@@ -22,7 +22,34 @@ function Square({ file, rank, imageUrl, piece }: SquareProps) {
 			key={`${file}-${rank}`}
 			className={`${isDark ? "bg-chessboard-square-dark" : "bg-chessboard-square-light"} aspect-square relative`}
 		>
-			<PieceImage imageUrl={imageUrl ?? null} piece={piece} file={file} rank={rank} />
+			<PieceImage
+				imageUrl={imageUrl ?? null}
+				piece={piece}
+				file={file}
+				rank={rank}
+			/>
+
+			<span
+				className={clsx(
+					"absolute top-0 left-0 text-xs font-semibold p-1",
+					isDark
+						? "text-chessboard-square-light"
+						: "text-chessboard-square-dark",
+				)}
+			>
+				{rank}
+			</span>
+
+			<span
+				className={clsx(
+					"absolute bottom-0 right-0 text-xs font-semibold p-1",
+					isDark
+						? "text-chessboard-square-light"
+						: "text-chessboard-square-dark",
+				)}
+			>
+				{file}
+			</span>
 		</div>
 	);
 }
