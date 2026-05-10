ec36c4de0bc590170c2d3be22d5998f5b74d0b02
Teddi <teddikao@gmail.com>
2026-05-10 12:58:58 +0800

    Add tooltips to setup toolbar buttons

--- app/src/features/variants/variantEditor/setupEditor/components/SetupToolbar.tsx
+++ app/src/features/variants/variantEditor/setupEditor/components/SetupToolbar.tsx
@@ -1,9 +1,15 @@
 import { Button } from "@/components/ui/button";
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipTrigger,
+} from "@/components/ui/tooltip";
 import useVariantDraftStore from "@/features/variants/variantEditor/common/stores/variantDraft";
 import { IconRepeat, IconTrash } from "@tabler/icons-react";
 
 function SetupToolbar() {
-	const { setupRulesDraft, updateSetupRulesDraft, syncSetupRulesDraftToDB } = useVariantDraftStore();
+	const { setupRulesDraft, updateSetupRulesDraft, syncSetupRulesDraftToDB } =
+		useVariantDraftStore();
 	if (!setupRulesDraft) return null;
 
 	function handleClearBoard() {
@@ -17,13 +23,29 @@ function SetupToolbar() {
 
 	return (
 		<div className="flex flex-col justify-center gap-2 bg-white shadow-md shadow-gray-400 rounded-md py-2 px-1">
-			<Button onClick={handleClearBoard} variant="ghost" className="hover:bg-gray-300 p-1">
-				<IconTrash className="size-6" />
-			</Button>
+			<Tooltip>
+				<TooltipTrigger asChild>
+					<Button
+						onClick={handleClearBoard}
+						variant="ghost"
+						className="hover:bg-gray-300 p-1"
+					>
+						<IconTrash className="size-6" />
+					</Button>
+				</TooltipTrigger>
 
-			<Button variant="ghost" className="hover:bg-gray-300 p-1">
-				<IconRepeat className="size-6" />
-			</Button>
+				<TooltipContent side="right">Clear board</TooltipContent>
+			</Tooltip>
+
+			<Tooltip>
+				<TooltipTrigger asChild>
+					<Button variant="ghost" className="hover:bg-gray-300 p-1">
+						<IconRepeat className="size-6" />
+					</Button>
+				</TooltipTrigger>
+
+				<TooltipContent side="right">Flip board</TooltipContent>
+			</Tooltip>
 		</div>
 	);
 }
