7ae62e1f37789dc5d3332c172007ceca2760553e
Teddi <teddikao@gmail.com>
2026-05-15 14:58:42 +0800

    Update variant list dialog to extend upon variant selection dialog

--- app/src/features/variants/variantListing/components/VariantListDialog.tsx
+++ app/src/features/variants/variantListing/components/VariantListDialog.tsx
@@ -1,22 +1,15 @@
-import {
-	Dialog,
-	DialogContent,
-	DialogDescription,
-	DialogFooter,
-	DialogHeader,
-	DialogTitle,
-} from "@/components/ui/dialog";
+
 import useVariantsStore from "@/features/variants/common/stores/variantsStore";
 import useVariantListDialogStore from "@/features/variants/variantListing/stores/variantListDialog";
-import { Button } from "@/components/ui/button";
 import DeleteVariantAlert from "@/features/variants/variantListing/components/DeleteVariantAlert";
 import useVariantDeleteAlertStore from "@/features/variants/variantListing/stores/variantDeleteAlert";
 import VariantRenameDialog from "@/features/variants/variantListing/components/VariantRenameDialog";
 import useVariantRenameDialogStore from "@/features/variants/variantListing/stores/variantRenameDialog";
 import { useNavigate } from "react-router-dom";
+import VariantSelectionDialog, { type VariantActions } from "@/features/variants/common/components/VariantSelectionDialog";
 
 function VariantListDialog() {
-	const { variants, hasHydrated } = useVariantsStore();
+	const { hasHydrated } = useVariantsStore();
 	const {
 		isOpen,
 		openDialog: openVariantListDialog,
@@ -43,80 +36,40 @@ function VariantListDialog() {
 		}
 	}
 
+	const actions: VariantActions = {
+		open: {
+			label: "Open",
+			variant: "default",
+			execute: (variantId) => {
+				closeVariantListDialog();
+				navigate(`variants/${variantId}`);
+			}
+		},
+		rename: {
+			label: "Rename",
+			variant: "outline",
+			execute: (variantId) => {
+				openVariantRenameDialog(variantId);
+			}
+		},
+		delete: {
+			label: "Delete",
+			variant: "destructive",
+			execute: (variantId) => {
+				openAlert(variantId);
+			}
+		}
+	}
+
 	return (
 		<>
-			<Dialog
-				open={isOpen}
-				onOpenChange={(open) =>
-					open ? openVariantListDialog() : closeVariantListDialog()
-				}
-			>
-				<DialogContent>
-					<DialogHeader>
-						<DialogTitle>My variants</DialogTitle>
-						<DialogDescription>
-							Below is a list of your created variants
-						</DialogDescription>
-					</DialogHeader>
-
-					<div className="flex flex-col gap-2">
-						{Object.entries(variants).map(
-							([variantId, variantInfo]) => (
-								<button
-									type="button"
-									onClick={() =>
-										handleVariantSelection(variantId)
-									}
-									className={
-										selectedVariantId === variantId
-											? "flex flex-row gap-2 p-2 rounded-md border-primary border-2 bg-(--muted-primary) [--tw-shadow-color:var(--shadow-primary)] shadow-md"
-											: "flex flex-row gap-2 shadow-md p-2 rounded-md bg-gray-300 shadow-gray-600"
-									}
-									aria-pressed={
-										selectedVariantId === variantId
-									}
-									key={variantId}
-								>
-									<span>{variantInfo.variantName}</span>
-								</button>
-							),
-						)}
-					</div>
-
-					{selectedVariantId && variants[selectedVariantId] && (
-						<DialogFooter>
-							<Button
-								onClick={() => {
-									closeVariantListDialog();
-									navigate(`variants/${selectedVariantId}`);
-								}}
-								className="px-4"
-								type="button"
-							>
-								Open
-							</Button>
-							<Button
-								onClick={() =>
-									openVariantRenameDialog(selectedVariantId)
-								}
-								variant="outline"
-								className="px-4"
-								type="button"
-							>
-								Rename
-							</Button>
-							<Button
-								className="px-4"
-								type="button"
-								variant="destructive"
-								onClick={() => openAlert(selectedVariantId)}
-							>
-								Delete
-							</Button>
-						</DialogFooter>
-					)}
-				</DialogContent>
-			</Dialog>
+			<VariantSelectionDialog
+				actions={actions}
+				isOpen={isOpen}
+				onOpenChange={(isOpen) => isOpen ? openVariantListDialog() : closeVariantListDialog()}
+				handleVariantSelection={handleVariantSelection}
+				selectedVariantId={selectedVariantId}
+			/>
 
 			<DeleteVariantAlert />
 			<VariantRenameDialog />
