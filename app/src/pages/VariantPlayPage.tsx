1f774aa158d990dbc15b59bf64b29734200fe5b6
Teddi <teddikao@gmail.com>
2026-05-18 17:10:25 +0800

    Add functionality to go back to home page from play page

--- app/src/pages/VariantPlayPage.tsx
+++ app/src/pages/VariantPlayPage.tsx
@@ -1,7 +1,24 @@
+import { Button } from "@/components/ui/button";
+import { IconChevronLeft } from "@tabler/icons-react";
+import { useNavigate } from "react-router-dom";
+
 function VariantPlayPage() {
+	const navigate = useNavigate();
+
+	function handleBackToHomePage() {
+		navigate("/");
+	}
+
 	return (
-		<h1>Variant Play Page</h1>
-	)
+		<div className="flex flex-col w-full h-full">
+			<div className="flex flex-row items-center gap-2 w-full p-3 pb-0">
+				<Button variant="ghost" className="pl-1 pr-2" data-icon="inline-start" onClick={handleBackToHomePage}>
+					<IconChevronLeft className="size-5" />
+					<span className="text-base font-normal">Back</span>
+				</Button>
+			</div>
+		</div>
+	);
 }
 
 export default VariantPlayPage;
