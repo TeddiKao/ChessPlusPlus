0d38e8e454e9c8df35fd222f4adfa28c50a70611
Teddi <teddikao@gmail.com>
2026-05-18 18:39:31 +0800

    Add state to store to track game board

--- app/src/features/variants/variantPlay/stores/playChessboard.ts
+++ app/src/features/variants/variantPlay/stores/playChessboard.ts
@@ -1,12 +1,21 @@
+import type { GameStateMap } from "@/features/variants/common/types/setupRules";
 import { create } from "zustand";
 
 type PlayChessboardStore = {
+	gameBoardState: GameStateMap | null;
+	updateGameBoardState: (gameBoardState: GameStateMap) => void;
+	resetGameBoardState: () => void;
+
 	isFlipped: boolean;
 	toggleBoardFlip: () => void;
 	resetBoardFlip: () => void;
 }
 
 const usePlayChessboardStore = create<PlayChessboardStore>((set) => ({
+	gameBoardState: null,
+	updateGameBoardState: (gameBoardState: GameStateMap) => set({ gameBoardState }),
+	resetGameBoardState: () => set({ gameBoardState: null }),
+	
 	isFlipped: false,
 	toggleBoardFlip: () => set((state) => ({ isFlipped: !state.isFlipped })),
 	resetBoardFlip: () => set({ isFlipped: false }),
