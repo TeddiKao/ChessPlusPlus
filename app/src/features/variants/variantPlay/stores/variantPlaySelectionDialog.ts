4c413ff139d44e7aea07d24e6211b5c098c3cd20
Teddi <teddikao@gmail.com>
2026-05-18 16:59:12 +0800

    Add state to track selected variant in play selection dialog

--- app/src/features/variants/variantPlay/stores/variantPlaySelectionDialog.ts
+++ app/src/features/variants/variantPlay/stores/variantPlaySelectionDialog.ts
@@ -4,12 +4,20 @@ type VariantPlaySelectionDialogStore = {
 	isVariantPlaySelectionDialogOpen: boolean;
 	openVariantPlaySelectionDialog: () => void;
 	closeVariantPlaySelectionDialog: () => void;
+
+	selectedVariantId: string | null;
+	updateSelectedVariantId: (variantId: string) => void;
+	clearSelectedVariantId: () => void;
 }
 
 const useVariantPlaySelectionDialogStore = create<VariantPlaySelectionDialogStore>((set) => ({
 	isVariantPlaySelectionDialogOpen: false,
 	openVariantPlaySelectionDialog: () => set({ isVariantPlaySelectionDialogOpen: true }),
 	closeVariantPlaySelectionDialog: () => set({ isVariantPlaySelectionDialogOpen: false }),
+
+	selectedVariantId: null,
+	updateSelectedVariantId: (variantId) => set({ selectedVariantId: variantId }),
+	clearSelectedVariantId: () => set({ selectedVariantId: null }),
 }));
 
 export default useVariantPlaySelectionDialogStore;
